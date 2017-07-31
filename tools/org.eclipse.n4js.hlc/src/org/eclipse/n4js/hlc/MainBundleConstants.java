345cc3d000bccf3c9c22f4fe049f2825bfb44204
Jakub Siberski <jakub.siberski@numberfour.eu>
2017-07-31 15:16:36 +0200

    [96] remove obsolete fragment from the product, fix bundle name

--- tools/org.eclipse.n4js.hlc/src/org/eclipse/n4js/hlc/MainBundleConstants.java
+++ tools/org.eclipse.n4js.hlc/src/org/eclipse/n4js/hlc/MainBundleConstants.java
@@ -1,16 +1,32 @@
 package org.eclipse.n4js.hlc;
 
+import org.eclipse.n4js.hlc.base.N4jscBase;
+
 /**
  * Constants that help to identify main product bundle and method to invoke. This is used in reflection calls to avoid
  * pollution of the pom.xml or MANIFEST.MF of the launcher.
  */
 final class MainBundleConstants {
 
+	private static String calculatedBundleName;
+	private static String calulatedClassName;
+
+	/**
+	 * We could hardcoded names, but dynamically calculating them makes this a bit more maintainable. Also it is easier
+	 * to find references etc.
+	 */
+	static {
+		Class<?> appClass = N4jscBase.class;
+		String className = appClass.getSimpleName();
+		calulatedClassName = appClass.getCanonicalName();
+		calculatedBundleName = calulatedClassName.substring(0, className.length());
+	}
+
 	/** regex pattern used to identify main product bundle */
-	final static String MAIN_BUNDLE_NAME_PATTERN = "app.*";
-	/** name of the method to invoke */
-	final static String MAIN_BUNDLE_METHOD_NAME = "main";
+	final static String MAIN_BUNDLE_NAME_PATTERN = calculatedBundleName + "*";
 	/** fqn of the class with method to invoke */
-	final static String MAIN_BUNDLE_CLASS_FQN = "app.Application";
+	final static String MAIN_BUNDLE_CLASS_FQN = calulatedClassName;
+	/** name of the method to invoke */
+	final static String MAIN_BUNDLE_METHOD_NAME = "main"; // hardcoded :/
 
 }
