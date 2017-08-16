6e3aba3d8b83832e556b38c27b66499114e756e5
mmews <marcus.mews@numberfour.eu>
2017-08-16 12:18:30 +0200

    #120: small enhancements of edge drawing

--- plugins/org.eclipse.n4js.smith.graph/src/org/eclipse/n4js/smith/graph/graph/CFEdge.java
+++ plugins/org.eclipse.n4js.smith.graph/src/org/eclipse/n4js/smith/graph/graph/CFEdge.java
@@ -59,21 +59,32 @@ public class CFEdge extends Edge {
 
 		Point src = srcN.getCenter();
 		Point tgt = tgtN.getCenter();
-		Point ctr = getArcPoint(src, tgt);
+		Point ctr = getArcControlPoint(src, tgt);
 
-		Point srcB = GraphUtils.pointOnRect(ctr.x, ctr.y, srcN.x, srcN.y, srcN.x + srcN.width, srcN.y + srcN.height);
-		Point tgtB = GraphUtils.pointOnRect(ctr.x, ctr.y, tgtN.x, tgtN.y, tgtN.x + tgtN.width, tgtN.y + tgtN.height);
+		Point srcB = GraphUtils.pointOnRect(ctr.x, ctr.y, srcN.x - 2, srcN.y - 2, srcN.x + srcN.width + 1,
+				srcN.y + srcN.height + 1);
+		Point tgtB = GraphUtils.pointOnRect(ctr.x, ctr.y, tgtN.x - 2, tgtN.y - 2, tgtN.x + tgtN.width + 1,
+				tgtN.y + tgtN.height + 1);
 
 		GraphUtils.arc(gc, ctr, srcB, tgtB);
 
 		drawArrowHead(gc, tgt, tgtB);
 	}
 
-	private Point getArcPoint(Point src, Point tgt) {
+	/**
+	 * Given there is a connecting line from src to tgt, this method returns a point ctr that lies between src and tgt
+	 * points, moved perpendicular away from that connecting line to the left the side. If the flipping mode is active,
+	 * the point ctr will be moved to the right side.
+	 */
+	private Point getArcControlPoint(Point src, Point tgt) {
+		// when flipped, the control point is on the other side of the edge,
+		// so that the arc will bow counter clock wise
+		boolean flip = src.x > tgt.x && src.y < tgt.y;
+		float flipper = flip ? -1 : 1;
 		float diffX = (src.x - tgt.x) / 2;
 		float diffY = (src.y - tgt.y) / 2;
-		float ctrX = src.x - diffX - diffY / 2;
-		float ctrY = src.y - diffY + diffX / 2;
+		float ctrX = src.x - diffX - flipper * diffY / 2;
+		float ctrY = src.y - diffY + flipper * diffX / 2;
 		Point ctr = new Point(ctrX, ctrY);
 		return ctr;
 	}
@@ -81,8 +92,21 @@ public class CFEdge extends Edge {
 	private void drawArrowHead(GC gc, Point tgt, Point tgtB) {
 		float xx = (tgt.x - tgtB.x) / 2;
 		float yy = (tgt.y - tgtB.y) / 2;
-		Point arrP1 = new Point(tgtB.x - yy - xx, tgtB.y + xx - yy);
-		Point arrP2 = new Point(tgtB.x + yy - xx, tgtB.y - xx - yy);
+
+		float p1x = -yy - xx;
+		float p1y = +xx - yy;
+		float p2x = +yy - xx;
+		float p2y = -xx - yy;
+
+		// This is a correction factor to unify the lengths of the arrow heads
+		float l = (float) (7 / Math.sqrt(p1x * p1x + p1y * p1y));
+		p1x *= l;
+		p1y *= l;
+		p2x *= l;
+		p2y *= l;
+
+		Point arrP1 = new Point(tgtB.x + p1x, tgtB.y + p1y);
+		Point arrP2 = new Point(tgtB.x + p2x, tgtB.y + p2y);
 		gc.drawLine((int) tgtB.x, (int) tgtB.y, (int) arrP1.x, (int) arrP1.y);
 		gc.drawLine((int) tgtB.x, (int) tgtB.y, (int) arrP2.x, (int) arrP2.y);
 	}
