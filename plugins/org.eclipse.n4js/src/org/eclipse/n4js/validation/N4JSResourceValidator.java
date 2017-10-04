dc18794f31c5df3834307fbca6963462e17f8cd0
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2017-10-04 14:10:47 +0200

    232: 1) avoid follow-up exceptions, 2) show post-processing failure
    In case post-processing fails with an exception/error being thrown:
    1) avoid multitude of follow-up exceptions in validation.
    2) show a single error in the editor / problems view.

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/validation/N4JSResourceValidator.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/validation/N4JSResourceValidator.java
@@ -10,6 +10,7 @@
  */
 package org.eclipse.n4js.validation;
 
+import java.util.Collections;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EObject;
@@ -20,6 +21,7 @@ import org.eclipse.xtext.service.OperationCanceledManager;
 import org.eclipse.xtext.util.CancelIndicator;
 import org.eclipse.xtext.util.IAcceptor;
 import org.eclipse.xtext.validation.CheckMode;
+import org.eclipse.xtext.validation.CheckType;
 import org.eclipse.xtext.validation.Issue;
 import org.eclipse.xtext.validation.ResourceValidatorImpl;
 
@@ -40,6 +42,37 @@ public class N4JSResourceValidator extends ResourceValidatorImpl {
 	private IN4JSCore n4jsCore;
 	@Inject
 	private OperationCanceledManager operationCanceledManager;
+
+	@Override
+	public List<Issue> validate(Resource resource, CheckMode mode, CancelIndicator cancelIndicator) {
+		if (resource instanceof N4JSResource) {
+			final N4JSResource resourceCasted = (N4JSResource) resource;
+			try {
+				resourceCasted.performPostProcessing(cancelIndicator);
+			} catch (Throwable th) {
+				// ignore this exception/error (we will create an issue for it below)
+			}
+			if (resourceCasted.isFullyProcessed()
+					&& resourceCasted.getPostProcessingThrowable() != null) {
+				// When getting here, we have an attempt to validate a resource that was post-processed but the
+				// post-processing failed (i.e. postProcessingThrowable!=null). Validating such a resource will usually
+				// cause a multitude of follow-up exceptions in many @Check methods (could easily be hundreds). Since,
+				// EMF/Xtext has the behavior of not aborting the overall validation due to an exception in a single
+				// @Check method, this will lead to many exceptions, which are all just follow-up issues of the problem
+				// that caused the post-processing to fail.
+				// 1) Since this is annoying and misleading, we just ignore all ordinary validation.
+				// 2) To not overlook the problem, we create a single validation issue (error) pointing out the problem.
+				// (for a test, see class AvoidFollowUpExceptionsInValidationTest)
+				final Throwable th = resourceCasted.getPostProcessingThrowable();
+				if (operationCanceledManager.isOperationCanceledException(th)) {
+					return Collections.emptyList(); // do not show errors in case of cancellation
+				}
+				final Issue issue = createPostProcessingFailedError(resource, th);
+				return Collections.singletonList(issue);
+			}
+		}
+		return super.validate(resource, mode, cancelIndicator);
+	}
 
 	/**
 	 * Don't validate the inferred module since all validation information should be available on the AST elements.
@@ -62,5 +95,22 @@ public class N4JSResourceValidator extends ResourceValidatorImpl {
 
 			// UtilN4.takeSnapshotInGraphView("post validation", resource);
 		}
+	}
+
+	private Issue createPostProcessingFailedError(Resource res, Throwable th) {
+		final String thKind = th instanceof Error ? "error" : (th instanceof Exception ? "exception" : "throwable");
+		final String thName = th.getClass().getSimpleName();
+		final String msg = IssueCodes.getMessageForPOST_PROCESSING_FAILED(thKind, thName, th.getMessage());
+		final Issue.IssueImpl issue = new Issue.IssueImpl();
+		issue.setCode(IssueCodes.POST_PROCESSING_FAILED);
+		issue.setSeverity(IssueCodes.getDefaultSeverity(IssueCodes.POST_PROCESSING_FAILED));
+		issue.setMessage(msg);
+		issue.setUriToProblem(res.getURI());
+		issue.setType(CheckType.NORMAL);
+		issue.setOffset(0);
+		issue.setLength(0);
+		issue.setLineNumber(0);
+		issue.setColumn(0);
+		return issue;
 	}
 }
