a36fc6b3c1ad5b528747d5aca292e022a54ffffa
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2017-10-11 15:03:46 +0200

    Avoid unnecessary refresh operation in compiler
    Signed-off-by: Sebastian Zarnekow <sebastian.zarnekow@gmail.com>

--- plugins/org.eclipse.n4js.ui/src/org/eclipse/n4js/ui/building/FileSystemAccessWithoutTraceFileSupport.java
+++ plugins/org.eclipse.n4js.ui/src/org/eclipse/n4js/ui/building/FileSystemAccessWithoutTraceFileSupport.java
@@ -10,8 +10,12 @@
  */
 package org.eclipse.n4js.ui.building;
 
+import org.eclipse.core.resources.IContainer;
 import org.eclipse.core.resources.IFile;
+import org.eclipse.core.runtime.IProgressMonitor;
+import org.eclipse.core.runtime.Path;
 import org.eclipse.xtext.builder.EclipseResourceFileSystemAccess2;
+import org.eclipse.xtext.generator.OutputConfiguration;
 
 /**
  * A file system access implementation that completely disables trace file support to avoid all the unnecessary overhead
@@ -21,6 +25,20 @@ public class FileSystemAccessWithoutTraceFileSupport extends EclipseResourceFile
 
 	@Override
 	protected IFile getTraceFile(IFile file) {
+		return null;
+	}
+
+	@Override
+	protected IFile getFile(String fileName, String outputName, IProgressMonitor progressMonitor) {
+		OutputConfiguration configuration = getOutputConfig(outputName);
+		IContainer container = getContainer(configuration);
+		if (container != null) {
+			// no need to refresh again - it was already done in
+			// org.eclipse.n4js.ui.building.N4JSBuilderParticipant#refreshOutputFolders
+			// not a life changer, but shaves off approx 5% from the build time and progress monitor flickering is
+			// reduced
+			return container.getFile(new Path(fileName));
+		}
 		return null;
 	}
 
