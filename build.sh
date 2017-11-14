9c4133581e3cd125efdd0f57a2bd5a2107731b08
Gunnar Bastkowski <gunnar.bastkowski@enfore.com>
2017-11-14 13:55:01 +0100

    CD-631 Enable usage of --user flag in docker run
    Signed-off-by: Gunnar Bastkowski <gunnar.bastkowski@enfore.com>

--- build.sh
+++ build.sh
@@ -9,7 +9,8 @@
 docker build -t n4js-build docker-build/
 
 # run the docker container
-docker run -ti --rm \
-    -v ~/.m2:/root/.m2/ \
+docker run -ti --rm      \
+    -u `id -u`:`id -g`   \
+    -v ~/.m2:/root/.m2/  \
     -v $(pwd):/workspace \
     n4js-build $@
