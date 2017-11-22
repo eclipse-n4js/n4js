443a64250d2709bac151640da076967987edcc5a
mmews <marcus.mews@numberfour.eu>
2017-11-22 12:40:39 +0100

    made N4JSX tests a new ui test project

---
+++ tests/org.eclipse.n4js.flowgraphs.ui.tests/src/org/eclipse/n4js/flowgraphs/ui/tests/N4JSXFlowgraphTest.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright (c) 2016 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.flowgraphs.ui.tests;
+
+import org.eclipse.n4js.xpect.methods.FindReferencesXpectMethod;
+import org.eclipse.n4js.xpect.methods.FlowgraphsXpectMethod;
+import org.eclipse.n4js.xpect.methods.TypeXpectMethod;
+import org.eclipse.n4js.xpect.methods.scoping.ScopeXpectMethod;
+import org.eclipse.n4js.xpect.ui.methods.OutputXpectMethod;
+import org.eclipse.n4js.xpect.validation.suppression.SuppressIssuesSetup;
+import org.junit.runner.RunWith;
+import org.xpect.XpectImport;
+import org.xpect.runner.XpectRunner;
+import org.xpect.runner.XpectSuiteClasses;
+import org.xpect.runner.XpectTestFiles;
+import org.xpect.runner.XpectTestFiles.FileRoot;
+import org.xpect.xtext.lib.tests.LinkingTest;
+import org.xpect.xtext.lib.tests.ResourceDescriptionTest;
+import org.xpect.xtext.lib.tests.ValidationTest;
+
+/**
+ * Plugin linking test
+ */
+@XpectSuiteClasses({
+		FlowgraphsXpectMethod.class,
+		LinkingTest.class,
+		TypeXpectMethod.class,
+		ScopeXpectMethod.class,
+		ResourceDescriptionTest.class,
+		ValidationTest.class,
+		OutputXpectMethod.class,
+		FindReferencesXpectMethod.class
+})
+@RunWith(XpectRunner.class)
+@XpectTestFiles(relativeTo = FileRoot.PROJECT, baseDir = "model", fileExtensions = "xt")
+@XpectImport({ SuppressIssuesSetup.class })
+public class N4JSXFlowgraphTest {
+	//
+}
