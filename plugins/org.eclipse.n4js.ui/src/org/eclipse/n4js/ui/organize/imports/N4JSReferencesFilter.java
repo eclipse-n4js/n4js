01e1f2784bae2e589bc71ebd5eb2c8ec45677216
Jakub Siberski <jakub.siberski@numberfour.eu>
2017-11-24 12:24:32 +0100

    mass move n4jsx.ui code

--- plugins/org.eclipse.n4js.ui/src/org/eclipse/n4js/ui/organize/imports/N4JSReferencesFilter.java
+++ plugins/org.eclipse.n4js.ui/src/org/eclipse/n4js/ui/organize/imports/N4JSReferencesFilter.java
@@ -10,6 +10,13 @@
  */
 package org.eclipse.n4js.ui.organize.imports;
 
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.n4js.n4JS.JSXElementName;
+import org.eclipse.n4js.n4JS.JSXPropertyAttribute;
+import org.eclipse.n4js.utils.ResourceType;
+
+import com.google.common.base.Strings;
+
 /**
  * N4JS is not doing any extra filtering on proxy cross refs, but sub languages might need to take advantage of this
  * variation point.
@@ -18,6 +25,29 @@ public class N4JSReferencesFilter implements IReferenceFilter {
 
 	@Override
 	public boolean test(ReferenceProxyInfo proxyInfo) {
-		return true;
+		ResourceType resourceType = ResourceType.getResourceType(proxyInfo.eobject);
+		switch (resourceType) {
+		case N4JSX:
+			return filterN4JSX(proxyInfo);
+
+		default:
+			return true;
+		}
+	}
+
+	/**
+	 * If reference is using name that starts with lower case it looks like HTML tag, if on top of that it is part of
+	 * {@link JSXElementName} or {@link JSXPropertyAttribute} than this reference should not be subject to Organize
+	 * Imports.
+	 */
+	private boolean filterN4JSX(ReferenceProxyInfo proxyInfo) {
+		// only if reference is looks like HTML tag
+		String usedName = proxyInfo.name;
+		boolean isLikeHTML = Strings.isNullOrEmpty(usedName) ? false : Character.isLowerCase(usedName.charAt(0));
+
+		// is reference part of JSX element
+		EObject refContainer = proxyInfo.eobject.eContainer();
+		return isLikeHTML && (refContainer == null ? false
+				: refContainer instanceof JSXElementName || refContainer instanceof JSXPropertyAttribute);
 	}
 }
