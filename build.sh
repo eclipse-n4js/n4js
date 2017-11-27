97f7f4d4d86a401756a11034d943c13895226ab5
Gunnar Bastkowski <gunnar.bastkowski@enfore.com>
2017-11-27 17:24:34 +0100

    CD-631 Add mount to include ~/.ssh from the host
    Signed-off-by: Gunnar Bastkowski <gunnar.bastkowski@enfore.com>

--- build.sh
+++ build.sh
@@ -2,23 +2,22 @@
 
 #
 # Small helper script to build the project inside a container.
-# Builds a Docker image from n4js-build/Dockerfile and executes it to
-# actually do the heavy build-lifting.
+#
+# Builds a Docker image from n4js-build/Dockerfile
+# and executes it
+# to actually do the heavy build-lifting.
 #
 # Usage:
 #   ./build.sh mvn package ...
-#
-# You may want to set the environment variable
-#    MAVEN_CENTRAL_URL
-# to a local/company Maven proxy (nexus, artifactory, archiva) so speed up builds.
 
 docker build                                            \
        --build-arg MAVEN_CENTRAL_URL=$MAVEN_CENTRAL_URL \
        -t n4js-build                                    \
        docker-build/
 
-docker run -ti --rm      \
-    -u `id -u`:`id -g`   \
-    -v ~/.m2:/root/.m2/  \
-    -v $(pwd):/workspace \
-    n4js-build $@
+docker run -ti --rm         \
+    -v ~/.ssh2:/root/.ssh2/ \
+    -v ~/.m2:/root/.m2/     \
+    -v $(pwd):/workspace    \
+    n4js-build              \
+    $@
