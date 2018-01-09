0af2fae9700b8940abd1d472cdcbfdba81e26939
Jakub Siberski <jakub.siberski@numberfour.eu>
2018-01-09 16:00:39 +0100

    GH-394 refactor xpect output utilities

--- plugins/org.eclipse.n4js.xpect.ui/src/org/eclipse/n4js/xpect/ui/common/XpectN4JSES5TranspilerHelper.java
+++ plugins/org.eclipse.n4js.xpect.ui/src/org/eclipse/n4js/xpect/ui/common/XpectN4JSES5TranspilerHelper.java
@@ -10,18 +10,14 @@
  */
 package org.eclipse.n4js.xpect.ui.common;
 
-import static com.google.common.base.Preconditions.checkState;
 import static com.google.common.base.Splitter.on;
 import static com.google.common.collect.FluentIterable.from;
-import static com.google.common.collect.Lists.newArrayList;
 import static com.google.common.collect.Lists.newLinkedList;
 import static org.eclipse.n4js.utils.io.FileUtils.createDirectory;
 import static org.eclipse.n4js.utils.io.FileUtils.createNestedDirectory;
 
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -29,8 +25,7 @@ import java.util.ArrayList;
 import java.util.Collections;
 import java.util.LinkedList;
 import java.util.List;
-import java.util.Map;
-import java.util.concurrent.ExecutionException;
+import java.util.StringJoiner;
 
 import org.eclipse.core.runtime.Platform;
 import org.eclipse.emf.common.util.URI;
@@ -43,7 +38,6 @@ import org.eclipse.n4js.naming.N4JSQualifiedNameConverter;
 import org.eclipse.n4js.projectModel.IN4JSCore;
 import org.eclipse.n4js.projectModel.IN4JSProject;
 import org.eclipse.n4js.projectModel.IN4JSSourceContainer;
-import org.eclipse.n4js.runner.IExecutor;
 import org.eclipse.n4js.runner.RunConfiguration;
 import org.eclipse.n4js.runner.RunnerFrontEnd;
 import org.eclipse.n4js.runner.SystemLoaderInfo;
@@ -54,17 +48,10 @@ import org.eclipse.n4js.runner.ui.ChooseImplementationHelper;
 import org.eclipse.n4js.transpiler.es.EcmaScriptSubGenerator;
 import org.eclipse.n4js.xpect.common.ResourceTweaker;
 import org.eclipse.xpect.xtext.lib.setup.FileSetupContext;
-import org.eclipse.xtext.EcoreUtil2;
-import org.eclipse.xtext.diagnostics.Severity;
 import org.eclipse.xtext.resource.FileExtensionProvider;
 import org.eclipse.xtext.resource.XtextResource;
-import org.eclipse.xtext.util.CancelIndicator;
-import org.eclipse.xtext.validation.CheckMode;
-import org.eclipse.xtext.validation.IResourceValidator;
-import org.eclipse.xtext.validation.Issue;
 import org.junit.Assert;
 
-import com.google.common.base.Joiner;
 import com.google.common.base.Optional;
 import com.google.inject.Inject;
 
@@ -73,8 +60,6 @@ import com.google.inject.Inject;
  * {@link EcmaScriptSubGenerator} for compilation and {@link NodeRunner} for execution.
  */
 public class XpectN4JSES5TranspilerHelper {
-
-	private final static String NL = "\n";
 
 	/**
 	 * https://github.com/eclipse/n4js/issues/394
@@ -89,10 +74,13 @@ public class XpectN4JSES5TranspilerHelper {
 	private IN4JSCore core;
 
 	@Inject
-	private IResourceValidator resourceValidator;
+	private XpectN4JSES5GeneratorHelper xpectGenerator;
 
 	@Inject
 	private FileExtensionProvider fileExtensionProvider;
+
+	@Inject
+	private XpectOutputConfigRunner configRunner;
 
 	@Inject
 	private RunnerFrontEnd runnerFrontEnd;
@@ -181,7 +169,7 @@ public class XpectN4JSES5TranspilerHelper {
 			for (final Resource dep : from(dependencies).filter(r -> !r.getURI().equals(resource.getURI()))) {
 				if ("n4jsd".equalsIgnoreCase(dep.getURI().fileExtension())) {
 					compileImplementationOfN4JSDFile(artificialRoot.toPath(), errorResult, dep, options, replaceQuotes);
-				} else if (isCompilable(dep, errorResult)) {
+				} else if (xpectGenerator.isCompilable(dep, errorResult)) {
 					final Script script = (Script) dep.getContents().get(0);
 					createTempJsFileWithScript(artificialRoot.toPath(), script, options, replaceQuotes);
 				}
@@ -202,38 +190,7 @@ public class XpectN4JSES5TranspilerHelper {
 					systemLoader, artificialRoot.toPath().toString() + "/" + testScript.getModule().getProjectId());
 		}
 
-		return executeWithConfig(runConfig, decorateStdStreams);
-	}
-
-	private String executeWithConfig(final RunConfiguration runConfig, boolean decorateStdStreams) {
-		String executionResult;
-		List<String> combinedOutput = new ArrayList<>();
-		try {
-			Process process = runnerFrontEnd.run(runConfig, executor());
-			EngineOutput eo = captureOutput(process);
-
-			if (decorateStdStreams) {
-				combinedOutput.add("<==");
-				combinedOutput.add("stdout:");
-			}
-			combinedOutput.addAll(eo.getStdOut());
-			if (decorateStdStreams) {
-				combinedOutput.add("stderr:");
-			}
-			combinedOutput.addAll(eo.getErrOut());
-			if (decorateStdStreams) {
-				combinedOutput.add("==>");
-			}
-
-			executionResult = Joiner.on(NL).join(combinedOutput);
-		} catch (Exception e) {
-			executionResult = e.getMessage();
-			// TODO Debugging:
-			System.out.println("Error in N4js execution " + e);
-			e.printStackTrace();
-
-		}
-		return executionResult;
+		return configRunner.executeWithConfig(runConfig, decorateStdStreams);
 	}
 
 	/**
@@ -250,126 +207,6 @@ public class XpectN4JSES5TranspilerHelper {
 	}
 
 	/**
-	 * private Executor which is not redirecting out & err streams.
-	 */
-	private IExecutor executor() {
-		return new IExecutor() {
-			@Override
-			public Process exec(String[] cmdLine, File workingDirectory, Map<String, String> envp)
-					throws ExecutionException {
-
-				ProcessBuilder pb = new ProcessBuilder(cmdLine);
-				pb.environment().putAll(envp);
-				pb.directory(workingDirectory);
-
-				try {
-					return pb.start();
-				} catch (IOException e) {
-					throw new ExecutionException(e);
-				}
-			}
-		};
-	}
-
-	/**
-	 * Composes the name of the module as seen from the node-path. Is uses '/' as the path-separator, since it the path
-	 * is consumed from js-code.
-	 */
-	private String jsModulePathToRun(Script script) {
-		ArrayList<String> pathSegements = newArrayList(getCompiledFileBasePath(script));
-		pathSegements.addAll(moduleQualifiedNameSegments(script));
-		String fileToRun = Joiner.on('/').join(pathSegements);
-		return fileToRun;
-	}
-
-	/**
-	 * This operation is blocking (waits for process to finish).
-	 *
-	 * @param process
-	 *            the process to get the standard out and error from.
-	 *
-	 * @return output captured during process run
-	 */
-	protected EngineOutput captureOutput(Process process) throws IOException {
-		List<String> out = new ArrayList<>();
-		List<String> err = new ArrayList<>();
-		String outLine;
-		String errLine;
-		BufferedReader bri = null;
-		BufferedReader bre = null;
-		try {
-			bri = new BufferedReader(new InputStreamReader(process.getInputStream()));
-			while ((outLine = bri.readLine()) != null) {
-				out.add(outLine);
-				// log.debug(outLine);
-			}
-			bre = new BufferedReader(new InputStreamReader(process.getErrorStream()));
-			while ((errLine = bre.readLine()) != null) {
-				err.add(errLine);
-				System.err.println(errLine);
-			}
-		} catch (Exception e) {
-			System.err.println("Exception in Engine.captureOutput: " + e.getMessage());
-		} finally {
-			if (bri != null) {
-				bri.close();
-			}
-			if (bre != null) {
-				bre.close();
-			}
-		}
-		return new EngineOutput(out, err);
-	}
-
-	/**
-	 * Compiles provided Script. Can replace backslashes with single quote (for windows command line issues)
-	 *
-	 * @param depRoot
-	 *            script to transpile
-	 * @param options
-	 *            the {@link GeneratorOption}s to use during compilation.
-	 * @param replaceQuotes
-	 *            should replace quotes (only for windows)
-	 * @return string representation of compilation result
-	 */
-	public String compile(Script depRoot, GeneratorOption[] options, boolean replaceQuotes) {
-		final Resource resource = depRoot.eResource();
-		EcoreUtil2.resolveLazyCrossReferences(resource, CancelIndicator.NullImpl);
-		final AbstractSubGenerator generator = getGeneratorForResource(resource);
-		String compileResultStr = generator.getCompileResultAsText(depRoot, options);
-		if (replaceQuotes) {
-			// Windows Node.js has problems with " as it interprets it as ending of script to execute
-			compileResultStr = compileResultStr.replace("\"", "'");
-		}
-		return compileResultStr;
-	}
-
-	/**
-	 * Checks if given resource can be compiled. During the process validators for that resource are executed.
-	 *
-	 * @param resource
-	 *            {@link Resource} that will check
-	 * @param errorResult
-	 *            {@link StringBuilder} to which potential validation issues will be appended
-	 * @return {@code true} if there are no validation issues for resource and it can be compiled
-	 */
-	public boolean isCompilable(Resource resource, StringBuilder errorResult) {
-		final AbstractSubGenerator generator = getGeneratorForResource(resource);
-		// shouldBeCompiled already calls the resource validator, so
-		// registerErrors should only be called when there have been errors found before
-		return generator.shouldBeCompiled(resource, CancelIndicator.NullImpl) || !registerErrors(resource, errorResult);
-	}
-
-	/**
-	 * Returns the desired generator instance for the resource argument. For now, will always return an instance of
-	 * {@link EcmaScriptSubGenerator}.
-	 */
-	private AbstractSubGenerator getGeneratorForResource(Resource resource) {
-		checkState(resource instanceof XtextResource, "Expected XtextResource was " + resource);
-		return ((XtextResource) resource).getResourceServiceProvider().get(EcmaScriptSubGenerator.class);
-	}
-
-	/**
 	 * Get dependent resources. Before calling this method make sure xpect configuration has been loaded.
 	 */
 	private List<Resource> getDependentResources() {
@@ -378,26 +215,6 @@ public class XpectN4JSES5TranspilerHelper {
 		List<Resource> resourcesFromXpectConfiguredResourceSet = new ArrayList<>();
 		resourcesFromXpectConfiguredResourceSet = readOutConfiguration.getResources();
 		return Collections.unmodifiableList(new ArrayList<>(resourcesFromXpectConfiguredResourceSet));
-	}
-
-	private boolean registerErrors(Resource dep, StringBuilder errorResult) {
-		boolean hasErrors = false;
-		List<Issue> issues = resourceValidator.validate(dep, CheckMode.ALL, CancelIndicator.NullImpl);
-		List<Issue> errorIssues = new ArrayList<>();
-		for (Issue issue : issues) {
-			if (Severity.ERROR == issue.getSeverity()) {
-				errorIssues.add(issue);
-			}
-		}
-		hasErrors = !errorIssues.isEmpty();
-		if (hasErrors) {
-			errorResult.append("Couldn't compile resource " + dep.getURI() + " because it contains errors: ");
-			for (Issue errorIssue : errorIssues) {
-				errorResult
-						.append(NL + errorIssue.getMessage() + " at line " + errorIssue.getLineNumber());
-			}
-		}
-		return hasErrors;
 	}
 
 	private void compileImplementationOfN4JSDFile(final Path root, StringBuilder errorResult, Resource dep,
@@ -428,7 +245,7 @@ public class XpectN4JSES5TranspilerHelper {
 						try {
 							Resource externalDep = dep.getResourceSet().getResource(potentialExternalSourceURI, true);
 							script = (Script) externalDep.getContents().get(0);
-							if (isCompilable(externalDep, errorResult)) {
+							if (xpectGenerator.isCompilable(externalDep, errorResult)) {
 								createTempJsFileWithScript(root, script, options, replaceQuotes);
 							}
 						} catch (Exception e) {
@@ -444,7 +261,7 @@ public class XpectN4JSES5TranspilerHelper {
 			final boolean replaceQuotes)
 			throws IOException {
 		// Compile script to get file content.
-		final String content = compile(script, options, replaceQuotes);
+		final String content = xpectGenerator.compile(script, options, replaceQuotes);
 
 		String srcgenSegments = getCompiledFileBasePath(script);
 		Path srcgenPath = createNestedDirectory(root, srcgenSegments);
@@ -476,6 +293,17 @@ public class XpectN4JSES5TranspilerHelper {
 	/** Splits up the script's qualified name along the delimiters. */
 	private LinkedList<String> moduleQualifiedNameSegments(final Script script) {
 		return newLinkedList(on(N4JSQualifiedNameConverter.DELIMITER).split(script.getModule().getQualifiedName()));
+	}
+
+	/**
+	 * Composes the name of the module as seen from the node-path. Is uses '/' as the path-separator, since it the path
+	 * is consumed from js-code.
+	 */
+	private String jsModulePathToRun(Script script) {
+		StringJoiner sj = new StringJoiner("/");
+		sj.add(getCompiledFileBasePath(script));
+		moduleQualifiedNameSegments(script).forEach(sj::add);
+		return sj.toString();
 	}
 
 	private String getCompiledFileBasePath(final Script script) {
