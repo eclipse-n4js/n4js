18ad850859bbe5d67448cd74977c5f14902fa1b0
Quang Tran <quang.tran@numberfour.eu>
2018-02-07 15:18:30 +0100

    Update code to Guava 18

--- tests/org.eclipse.n4js.ui.tests/src/org/eclipse/n4js/tests/projectModel/EclipseBasedProjectModelSetup.java
+++ tests/org.eclipse.n4js.ui.tests/src/org/eclipse/n4js/tests/projectModel/EclipseBasedProjectModelSetup.java
@@ -29,8 +29,6 @@ import org.eclipse.xtext.ui.XtextProjectHelper;
 import org.eclipse.xtext.util.StringInputStream;
 
 import com.google.common.base.Charsets;
-import com.google.common.io.CharStreams;
-import com.google.common.io.OutputSupplier;
 
 /**
  */
@@ -108,8 +106,7 @@ public class EclipseBasedProjectModelSetup extends AbstractProjectModelSetup {
 		zipOutputStream.putNextEntry(new ZipEntry("src/sub/leaf/D.js"));
 
 		zipOutputStream.putNextEntry(new ZipEntry(IN4JSProject.N4MF_MANIFEST));
-		// this will close the stream
-		CharStreams.write("ProjectId: " + host.archiveProjectId + "\n" +
+		zipOutputStream.write(("ProjectId: " + host.archiveProjectId + "\n" +
 				"ProjectType: library\n" +
 				"ProjectVersion: 0.0.1-SNAPSHOT\n" +
 				"VendorId: org.eclipse.n4js\n" +
@@ -120,13 +117,8 @@ public class EclipseBasedProjectModelSetup extends AbstractProjectModelSetup {
 				"	source { " +
 				"		\"src\"\n" +
 				"	}\n" +
-				"}\n", CharStreams.newWriterSupplier(new OutputSupplier<ZipOutputStream>() {
-					@Override
-					public ZipOutputStream getOutput() throws IOException {
-						return zipOutputStream;
-					}
-				}, Charsets.UTF_8));
-
+				"}\n").getBytes(Charsets.UTF_8));
+		zipOutputStream.close();
 		archiveFile.create(new ByteArrayInputStream(byteArrayOutputSteam.toByteArray()), false, null);
 
 		host.setArchiveFileURI(URI.createPlatformResourceURI(archiveFile.getFullPath().toString(), true));
