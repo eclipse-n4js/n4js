a145f8f6799faa52c92384e0267cc81bde8b1c9f
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2018-05-08 10:25:37 +0200

    GH-766: JSON xpect test bundle
    Signed-off-by: Luca Beurer-Kellner <lucabeurerkellner@gmail.com>

--- plugins/org.eclipse.n4js.json/src/org/eclipse/n4js/json/validation/validators/JSONValidator.java
+++ plugins/org.eclipse.n4js.json/src/org/eclipse/n4js/json/validation/validators/JSONValidator.java
@@ -10,7 +10,18 @@
  */
 package org.eclipse.n4js.json.validation.validators;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.eclipse.n4js.json.JSON.JSONObject;
+import org.eclipse.n4js.json.JSON.JSONPackage;
+import org.eclipse.n4js.json.JSON.JSONValue;
+import org.eclipse.n4js.json.JSON.NameValuePair;
 import org.eclipse.n4js.json.validation.AbstractResourceDependentJSONValidator;
+import org.eclipse.n4js.json.validation.IssueCodes;
+import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
+import org.eclipse.xtext.validation.Check;
 import org.eclipse.xtext.validation.ComposedChecks;
 
 /**
@@ -21,9 +32,22 @@ import org.eclipse.xtext.validation.ComposedChecks;
 })
 public class JSONValidator extends AbstractResourceDependentJSONValidator {
 	
-//	/** Checks the validity of a {@link JSONDocument} element. */
-//	@Check
-//	public void checkDocument(JSONDocument document) {
-//		// validate JSON here
-//	}
+	/**
+	 * Checks for duplicate keys in {@link JSONObject}s.
+	 */
+	@Check
+	public void checkDuplicateKeys(JSONObject object) {
+		final Map<String, JSONValue> values = new HashMap<>();
+		
+		for (NameValuePair pair : object.getNameValuePairs()) {
+			final JSONValue value = values.get(pair.getName());
+			if (value != null) {
+				final INode duplicatedNode = NodeModelUtils.findActualNodeFor(value);
+				final int duplicatedLine = NodeModelUtils.getLineAndColumn(duplicatedNode, duplicatedNode.getOffset()).getLine();
+				addIssue(IssueCodes.getMessageForJSON_DUPLICATE_KEY(pair.getName(), duplicatedLine), pair, 
+						JSONPackage.Literals.NAME_VALUE_PAIR__NAME, IssueCodes.JSON_DUPLICATE_KEY);
+			}
+			values.put(pair.getName(), pair.getValue());
+		}
+	}
 }
