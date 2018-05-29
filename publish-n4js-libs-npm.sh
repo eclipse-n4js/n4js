0d517b190de51ccbac994598b2a0824b894789d3
Quang Tran <quang.tran@numberfour.eu>
2018-05-29 17:14:53 +0200

    Publish n4js-libs and n4js-cli to the staging area

--- publish-n4js-libs-npm.sh
+++ publish-n4js-libs-npm.sh
@@ -4,6 +4,9 @@ set -e
 
 echo "Start publishing n4js-libs and n4js-cli"
 
+# The first parameter is the npm registry url
+NPM_REGISTRY=$1
+
 # Navigate to n4js-libs folder
 cd `dirname $0`/n4js-libs
 
@@ -17,7 +20,7 @@ npm install
 echo "=== Run lerna run build to build n4js-cli"
 lerna run build
 
-# Run npm task script 'publish-canary'
-yarn run publish-canary
+# Run npm task script 'publish-canary' to publish n4js-libs and n4js-cli to local npm registry
+yarn run publish-canary $NPM_REGISTRY
 
 echo "End publishing n4js-libs and n4js-cli"
