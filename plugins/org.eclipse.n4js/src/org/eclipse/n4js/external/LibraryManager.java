be9e3efabd361003764a8d98705240f2ef6cb1d4
mmews-n4 <marcus.mews@numberfour.eu>
2018-06-01 15:42:19 +0200

    GH-862: Library Manager does not install types for transitive dependencies (#863)
    * fix to install also transitive dependencies in library manager
    
    * refactoring: return N4JSExternalProject
    
    * add comment for temporary solution. will be obsolete with pck.json

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/external/LibraryManager.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/external/LibraryManager.java
@@ -24,6 +24,7 @@ import java.net.URI;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
+import java.util.HashMap;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
@@ -45,6 +46,8 @@ import org.eclipse.n4js.binaries.IllegalBinaryStateException;
 import org.eclipse.n4js.binaries.nodejs.NpmBinary;
 import org.eclipse.n4js.external.LibraryChange.LibraryChangeType;
 import org.eclipse.n4js.external.libraries.PackageJson;
+import org.eclipse.n4js.n4mf.ProjectDependency;
+import org.eclipse.n4js.n4mf.ProjectDescription;
 import org.eclipse.n4js.projectModel.IN4JSCore;
 import org.eclipse.n4js.smith.ClosableMeasurement;
 import org.eclipse.n4js.smith.DataCollector;
@@ -196,12 +199,47 @@ public class LibraryManager {
 		try (ClosableMeasurement mes = dcLibMngr.getClosableMeasurement("installDependenciesInternal");) {
 
 			List<LibraryChange> actualChanges = installUninstallNPMs(monitor, status, versionedNPMs, emptyList());
+
 			indexSynchronizer.synchronizeNpms(monitor, actualChanges);
+
+			installDependenciesOfNPMs(monitor, actualChanges);
 
 			return status;
 
 		} finally {
 			monitor.done();
+		}
+	}
+
+	/**
+	 * This method will install all dependencies of the requested NPMs. Since it calls
+	 * {@link #installNPMsInternal(Map, IProgressMonitor)}, this method will install all transitive dependencies of the
+	 * requested NPMs.
+	 * <p>
+	 * GH-862: Please remove this after GH-821 is solved
+	 */
+	private void installDependenciesOfNPMs(IProgressMonitor monitor, List<LibraryChange> actualChanges) {
+		Map<String, String> dependencies = new HashMap<>();
+		for (LibraryChange libChange : actualChanges) {
+			if (libChange.type == LibraryChangeType.Added) {
+				N4JSExternalProject addedPrj = externalLibraryWorkspace.getProject(libChange.name);
+				if (addedPrj != null) {
+					ProjectDescription pd = externalLibraryWorkspace.getProjectDescription(libChange.location);
+					for (ProjectDependency pDep : pd.getProjectDependencies()) {
+						String name = pDep.getProjectId();
+						String version = NO_VERSION;
+						if (pDep.getVersionConstraint() != null) {
+							version = pDep.getVersionConstraint().toString();
+						}
+						dependencies.put(name, version);
+					}
+				}
+			}
+		}
+		if (!dependencies.isEmpty()) {
+			String msg = "Installing dependencies: " + String.join(", ", dependencies.keySet());
+			logger.logInfo(msg);
+			installNPMsInternal(dependencies, monitor);
 		}
 	}
 
