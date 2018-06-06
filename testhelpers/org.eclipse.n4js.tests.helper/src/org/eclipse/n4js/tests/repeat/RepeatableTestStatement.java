fd39e1b75f8bc1c9f6c0ae9fa6e6427ff01231cb
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2018-06-06 13:48:26 +0200

    Test helpers that allow to run tests in a loop
    Signed-off-by: Sebastian Zarnekow <sebastian.zarnekow@gmail.com>

---
+++ testhelpers/org.eclipse.n4js.tests.helper/src/org/eclipse/n4js/tests/repeat/RepeatableTestStatement.java
@@ -0,0 +1,35 @@
+/**
+ * Copyright (c) 2018 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.tests.repeat;
+
+import org.eclipse.n4js.internal.RaceDetectionHelper;
+import org.junit.runners.model.Statement;
+
+/**
+ *
+ */
+class RepeatableTestStatement extends Statement {
+	private final int times;
+	private final Statement statement;
+
+	RepeatableTestStatement(int times, Statement statement) {
+		this.times = times;
+		this.statement = statement;
+	}
+
+	@Override
+	public void evaluate() throws Throwable {
+		for (int i = 1; i <= times; i++) {
+			RaceDetectionHelper.log("Run %d of %d\n", i, times);
+			statement.evaluate();
+		}
+	}
+}
