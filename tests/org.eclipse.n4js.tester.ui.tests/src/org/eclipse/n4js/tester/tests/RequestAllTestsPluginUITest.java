190e281d847a72b967ed6e38b978a3fce4643885
mmews <marcus.mews@numberfour.eu>
2018-06-21 11:05:13 +0200

    start the server manually. switch to PluginUiTest

--- tests/org.eclipse.n4js.tester.ui.tests/src/org/eclipse/n4js/tester/tests/RequestAllTestsPluginUITest.java
+++ tests/org.eclipse.n4js.tester.ui.tests/src/org/eclipse/n4js/tester/tests/RequestAllTestsPluginUITest.java
@@ -10,8 +10,12 @@
  */
 package org.eclipse.n4js.tester.tests;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import javax.servlet.http.HttpServletResponse;
 
+import org.eclipse.n4js.tester.internal.TesterActivator;
 import org.eclipse.n4js.tester.server.HttpServerManager;
 import org.eclipse.n4js.tester.server.resources.ContentType;
 import org.eclipse.n4js.tester.server.resources.ResourceRouterServlet;
@@ -19,6 +23,7 @@ import org.eclipse.n4js.tests.builder.AbstractBuilderParticipantTest;
 import org.eclipse.xtext.ui.testing.util.IResourcesSetupUtil;
 import org.hamcrest.BaseMatcher;
 import org.hamcrest.Description;
+import org.junit.Before;
 import org.junit.Test;
 
 import com.jayway.restassured.RestAssured;
@@ -28,7 +33,34 @@ import com.jayway.restassured.response.ValidatableResponse;
  * This test triggers the tester servlet implementation and with it lazy injection. Hence, this test assures that our
  * fragile injector setup works in the UI case regarding the tester.
  */
-public class RequestAllTestsPluginTest extends AbstractBuilderParticipantTest {
+public class RequestAllTestsPluginUITest extends AbstractBuilderParticipantTest {
+
+	static final String HOST = "localhost";
+	static final int PORT = 9415;
+	static final String URL = "http://" + HOST + ":" + PORT + HttpServerManager.CONTEXT_ROOT
+			+ ResourceRouterServlet.CONTEXT_PATH;
+
+	private HttpServerManager serverManager;
+
+	/** Start the Jetty Server if not started yet. */
+	@Before
+	public void startServer() {
+		Map<String, Object> serverConfig = new HashMap<>();
+		serverConfig.put(HttpServerManager.HTTP_PORT, PORT);
+		serverManager = TesterActivator.getInjector().getInstance(HttpServerManager.class);
+		if (serverManager != null && !serverManager.isRunning(PORT)) {
+			serverManager.startServer(serverConfig);
+			// assertTrue("Server is not running on port " + PORT, serverManager.isRunning(PORT));
+		}
+	}
+
+	/** Stop the server. */
+	@Before
+	public void stopServer() {
+		if (serverManager != null && serverManager.isRunning(PORT)) {
+			serverManager.stopServer(PORT);
+		}
+	}
 
 	/**
 	 * This test triggers the tester servlet implementation and with it lazy injection.
@@ -36,11 +68,6 @@ public class RequestAllTestsPluginTest extends AbstractBuilderParticipantTest {
 	@Test
 	public void requestAllTestsFromIDE() throws Exception {
 		IResourcesSetupUtil.waitForBuild();
-
-		String HOST = "localhost";
-		int PORT = 9415;
-		String URL = "http://" + HOST + ":" + PORT + HttpServerManager.CONTEXT_ROOT
-				+ ResourceRouterServlet.CONTEXT_PATH;
 
 		ValidatableResponse then = RestAssured.given().contentType(ContentType.ASSEMBLE_TEST_CATALOG.toString())
 				.when().get(URL + "testcatalog")
