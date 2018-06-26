f663a322b976322d8fe9c38ea38d0c5df5c37b22
Quang Tran <quang.tran@numberfour.eu>
2018-06-26 12:17:00 +0200

    Fix syntax

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -17,7 +17,7 @@ cd `pwd -P`
 DIR_ROOT=`pwd`
 
 echo "We are currently in $PWD"
-export N4_N4JSC_JAR = "${DIR_ROOT}/tools/org.eclipse.n4js.hlc/target/n4jsc.jar"
+export N4_N4JSC_JAR="${DIR_ROOT}/tools/org.eclipse.n4js.hlc/target/n4jsc.jar"
 
 # DIRS contains all direct folders of packages
 DIRS=$(find ./packages/ -type d -mindepth 1 -maxdepth 1)
