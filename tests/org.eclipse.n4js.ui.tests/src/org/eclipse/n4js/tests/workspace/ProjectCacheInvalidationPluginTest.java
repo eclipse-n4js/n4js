d945c752ddf74bf3ad6f478ae838ceeba5940793
Luca Beurer-Kellner <lucabeurerkellner@gmail.com>
2018-07-23 13:46:34 +0200

    GH-984: add test case to test nested project hierarchies
    Signed-off-by: Luca Beurer-Kellner <lucabeurerkellner@gmail.com>

--- tests/org.eclipse.n4js.ui.tests/src/org/eclipse/n4js/tests/workspace/ProjectCacheInvalidationPluginTest.java
+++ tests/org.eclipse.n4js.ui.tests/src/org/eclipse/n4js/tests/workspace/ProjectCacheInvalidationPluginTest.java
@@ -13,6 +13,7 @@ package org.eclipse.n4js.tests.workspace;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
 import java.io.IOException;
 import java.util.function.Consumer;
 
@@ -35,11 +36,14 @@ import org.eclipse.n4js.projectModel.IN4JSCore;
 import org.eclipse.n4js.projectModel.IN4JSProject;
 import org.eclipse.n4js.tests.builder.AbstractBuilderParticipantTest;
 import org.eclipse.n4js.tests.util.PackageJSONTestUtils;
+import org.eclipse.n4js.tests.util.ProjectTestsUtils;
 import org.eclipse.n4js.ui.internal.EclipseBasedN4JSWorkspace;
+import org.eclipse.n4js.utils.io.FileCopier;
 import org.eclipse.xtext.ui.resource.XtextResourceSetProvider;
 import org.junit.Test;
 
 import com.google.common.base.Optional;
+import com.google.common.io.Files;
 import com.google.inject.Inject;
 
 /**
@@ -47,6 +51,10 @@ import com.google.inject.Inject;
  * resource change events.
  */
 public class ProjectCacheInvalidationPluginTest extends AbstractBuilderParticipantTest {
+
+	private static final String PROBANDS = "probands";
+
+	private static final String PROBANDS_SUBFOLDER = "GH-984-nested-projects";
 
 	@Inject
 	private XtextResourceSetProvider resourceSetProvider;
@@ -138,6 +146,82 @@ public class ProjectCacheInvalidationPluginTest extends AbstractBuilderParticipa
 		// tear down
 		testProject1.delete(true, new NullProgressMonitor());
 		testProject2.delete(true, new NullProgressMonitor());
+	}
+
+	/**
+	 * Creates three projects, one of which is the top-level in which the two other projects are nested.
+	 *
+	 * Performs an atomic package.json modification in both nested projects and asserts that the
+	 * {@link EclipseBasedN4JSWorkspace} correctly invalidates its cache for both projects and correctly reflects the
+	 * changes of both project description files.
+	 */
+	@Test
+	public void testUpdateNestedProject() throws CoreException, IOException {
+		// test case constants
+		final String updatedImplementationId1 = "updated1";
+		final String updatedImplementationId2 = "updated2";
+		final String updatedImplementationId3 = "updated3";
+
+		// setup probands
+		File projectsRoot = Files.createTempDir();
+		projectsRoot.deleteOnExit();
+		final File probandsLocation = new File(getResourceUri(PROBANDS, PROBANDS_SUBFOLDER));
+		FileCopier.copy(probandsLocation.toPath(), projectsRoot.toPath());
+
+		// setup workspace
+		final IProject nestedTestProject1 = ProjectTestsUtils.createProjectWithLocation(projectsRoot,
+				"TopLevel/packages/A_Nested", "A_Nested");
+		final IProject nestedTestProject2 = ProjectTestsUtils.createProjectWithLocation(projectsRoot,
+				"TopLevel/packages/Z_Nested", "Z_Nested");
+		final IProject topLevelTestProject = ProjectTestsUtils.createProjectWithLocation(projectsRoot,
+				"TopLevel", "TopLevel");
+
+		// perform package.json modification
+		runAtomicWorkspaceOperation(monitor -> {
+			final IFile packageJson1 = nestedTestProject1.getFile(IN4JSProject.PACKAGE_JSON);
+			final IFile packageJson2 = nestedTestProject2.getFile(IN4JSProject.PACKAGE_JSON);
+			final IFile packageJson3 = topLevelTestProject.getFile(IN4JSProject.PACKAGE_JSON);
+
+			// add implementation ID for both nested projects
+			updatePackageJsonFile(packageJson1,
+					o -> PackageJSONTestUtils.setImplementationId(o, updatedImplementationId1));
+			updatePackageJsonFile(packageJson2,
+					o -> PackageJSONTestUtils.setImplementationId(o, updatedImplementationId2));
+
+			// add implementation ID to top-level project
+			updatePackageJsonFile(packageJson3,
+					o -> PackageJSONTestUtils.setImplementationId(o, updatedImplementationId3));
+		});
+
+		// obtain workspace representation of the nested test projects
+		Optional<? extends IN4JSProject> projectHandle1 = n4jsCore
+				.findProject(URI.createPlatformResourceURI(nestedTestProject1.getFullPath().toString(), true));
+		Optional<? extends IN4JSProject> projectHandle2 = n4jsCore
+				.findProject(URI.createPlatformResourceURI(nestedTestProject2.getFullPath().toString(), true));
+		Optional<? extends IN4JSProject> projectHandle3 = n4jsCore
+				.findProject(URI.createPlatformResourceURI(topLevelTestProject.getFullPath().toString(), true));
+
+		// make assertions
+		assertTrue("Project handle for nested test project 1 can be obtained.", projectHandle1.isPresent());
+		assertTrue("Project handle for nested test project 2 can be obtained.", projectHandle2.isPresent());
+		assertTrue("Project handle for top-level test project can be obtained.", projectHandle3.isPresent());
+
+		assertTrue("Project handle for nested test project 1 should have implementation ID (cache was invalidated).",
+				projectHandle1.get().getImplementationId().isPresent());
+		assertTrue("Project handle for nested test project 2 should have implementation ID (cache was invalidated).",
+				projectHandle2.get().getImplementationId().isPresent());
+		assertTrue(
+				"Project handle for the top-level test project should have implementation ID (cache was invalidated).",
+				projectHandle3.get().getImplementationId().isPresent());
+
+		assertEquals(updatedImplementationId1, projectHandle1.get().getImplementationId().get());
+		assertEquals(updatedImplementationId2, projectHandle2.get().getImplementationId().get());
+		assertEquals(updatedImplementationId3, projectHandle3.get().getImplementationId().get());
+
+		// tear down
+		nestedTestProject1.delete(true, new NullProgressMonitor());
+		nestedTestProject2.delete(true, new NullProgressMonitor());
+		topLevelTestProject.delete(true, new NullProgressMonitor());
 	}
 
 	/**
