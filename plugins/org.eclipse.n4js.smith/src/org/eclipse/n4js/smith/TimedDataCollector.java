37f2d59f84363429f062f216d357982e6af04de8
mmews <marcus.mews@numberfour.eu>
2018-07-25 14:02:57 +0200

    add comment

--- plugins/org.eclipse.n4js.smith/src/org/eclipse/n4js/smith/TimedDataCollector.java
+++ plugins/org.eclipse.n4js.smith/src/org/eclipse/n4js/smith/TimedDataCollector.java
@@ -60,6 +60,7 @@ class TimedDataCollector extends DataCollector {
 		return (TimedMeasurement) getMeasurement(name);
 	}
 
+	/** This method must be synchronized to protect race conditions when calling data.add */
 	synchronized private void consume(TimedMeasurement measurement) {
 		TimedMeasurement timed = measurement;
 		data.add(new DataPoint(timed.name, timed.elapsed(NANOSECONDS)));
