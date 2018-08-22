c5e9a46a81cd4e5275f8d3ca5d2840b8ef7df476
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2018-08-22 17:27:01 +0200

    Introduced synchronized ContextFinder subtype to avoid races on EObjects
    Signed-off-by: Sebastian Zarnekow <sebastian.zarnekow@gmail.com>

--- plugins/org.eclipse.n4js.json/src/org/eclipse/n4js/json/JSONRuntimeModule.java
+++ plugins/org.eclipse.n4js.json/src/org/eclipse/n4js/json/JSONRuntimeModule.java
@@ -17,16 +17,19 @@ import org.eclipse.n4js.json.resource.JSONResourceDescriptionManager;
 import org.eclipse.n4js.json.resource.JSONResourceDescriptionStrategy;
 import org.eclipse.n4js.json.validation.JSONIssueCodes;
 import org.eclipse.n4js.json.validation.JSONIssueSeveritiesProvider;
+import org.eclipse.n4js.xtext.serializer.SynchronizedContextFinder;
 import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.naming.IQualifiedNameProvider;
 import org.eclipse.xtext.parsetree.reconstr.IHiddenTokenHelper;
 import org.eclipse.xtext.resource.IDefaultResourceDescriptionStrategy;
 import org.eclipse.xtext.resource.IResourceDescription;
+import org.eclipse.xtext.serializer.sequencer.ContextFinder;
 import org.eclipse.xtext.validation.IssueSeveritiesProvider;
 
 /**
  * Use this class to register components to be used at runtime / without the Equinox extension registry.
  */
+@SuppressWarnings("restriction")
 public class JSONRuntimeModule extends AbstractJSONRuntimeModule {
 	/** Bind custom value converter for JSON-specific literals */
 	@Override
@@ -58,5 +61,10 @@ public class JSONRuntimeModule extends AbstractJSONRuntimeModule {
 	/** Bind custom hidden token helper. */
 	public Class<? extends IHiddenTokenHelper> bindIHiddenTokenHelper() {
 		return JSONHiddenTokenHelper.class;
+	}
+
+	/** Avoid concurrent installation of adapter on EObjects in the grammar access instances */
+	public Class<? extends ContextFinder> bindContextFinder() {
+		return SynchronizedContextFinder.class;
 	}
 }
