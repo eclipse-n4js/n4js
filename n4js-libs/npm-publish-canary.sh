b8f72709499af48d411e8c06bef7f9b2293f3dda
Quang Tran <quang.tran@numberfour.eu>
2018-09-03 16:22:26 +0200

    GH-1105: Move the logics into the public case

--- n4js-libs/npm-publish-canary.sh
+++ n4js-libs/npm-publish-canary.sh
@@ -39,33 +39,31 @@ cleanup() {
 export NPM_CONFIG_GLOBALCONFIG="DIR_ROOT"
 echo "Publishing using .npmrc configuration to ${NPM_REGISTRY}";
 
-# We only publish if there are changes in n4js-libs since the last commit
-PKG_VERSION=`cat lerna.json  | jq -r '.version' | xargs -t semver -i minor {}`
-N4JS_LIBS_COMMIT_ID_LOCAL=`git log -1 --format="%H" ${DIR_ROOT} | cut -c1-8`
-
-# Check the latest commit on public npm registry, use n4js-node as a representative
-N4JS_LIBS_VERSION_PUBLIC=`curl -s ${NPM_REGISTRY}/n4js-node | jq -r '.["dist-tags"].latest'`
-echo "N4JS_LIBS_VERSION_PUBLIC=${N4JS_LIBS_VERSION_PUBLIC}"
-
-# The commit ID is the last 8 letters of public version
-N4JS_LIBS_COMMIT_ID_PUBLIC=${N4JS_LIBS_VERSION_PUBLIC: -8}
-
-# Stop if the local ID equals public ID
-if [ "${N4JS_LIBS_COMMIT_ID_LOCAL}" = "${N4JS_LIBS_COMMIT_ID_PUBLIC}" ]; then
-    echo "n4js-libs has NOT been changed since the last publish. Local commit ID = Public commit ID = ${N4JS_LIBS_COMMIT_ID_LOCAL}. n4js-libs will not be published."
-    exit 0
-else
-    echo "n4js-libs has been changed since the last publish. Local commit ID = ${N4JS_LIBS_COMMIT_ID_LOCAL} while public commit ID = ${N4JS_LIBS_COMMIT_ID_PUBLIC}"
-fi
-
-CURRENT_DATE=`date +%Y%m%d`
-
-PUBLISH_VERSION="${PKG_VERSION}-alpha.${CURRENT_DATE}.${N4JS_LIBS_COMMIT_ID_LOCAL}" 
-echo "PUBLISH_VERSION=${PUBLISH_VERSION}"
-
-
 if [ "${NPM_TAG}" = "latest" ]; then
-    echo "WE ARE HERE OK?"
+    # We only publish if there are changes in n4js-libs since the last commit
+    PKG_VERSION=`cat lerna.json  | jq -r '.version' | xargs -t semver -i minor {}`
+    N4JS_LIBS_COMMIT_ID_LOCAL=`git log -1 --format="%H" ${DIR_ROOT} | cut -c1-8`
+    
+    # Check the latest commit on public npm registry, use n4js-node as a representative
+    N4JS_LIBS_VERSION_PUBLIC=`curl -s ${NPM_REGISTRY}/n4js-node | jq -r '.["dist-tags"].latest'`
+    echo "N4JS_LIBS_VERSION_PUBLIC=${N4JS_LIBS_VERSION_PUBLIC}"
+    
+    # The commit ID is the last 8 letters of public version
+    N4JS_LIBS_COMMIT_ID_PUBLIC=${N4JS_LIBS_VERSION_PUBLIC: -8}
+    
+    # Stop if the local ID equals public ID
+    if [ "${N4JS_LIBS_COMMIT_ID_LOCAL}" = "${N4JS_LIBS_COMMIT_ID_PUBLIC}" ]; then
+        echo "n4js-libs has NOT been changed since the last publish. Local commit ID = Public commit ID = ${N4JS_LIBS_COMMIT_ID_LOCAL}. n4js-libs will not be published."
+        exit 0
+    else
+        echo "n4js-libs has been changed since the last publish. Local commit ID = ${N4JS_LIBS_COMMIT_ID_LOCAL} while public commit ID = ${N4JS_LIBS_COMMIT_ID_PUBLIC}"
+    fi
+
+    CURRENT_DATE=`date +%Y%m%d`
+
+    PUBLISH_VERSION="${PKG_VERSION}-alpha.${CURRENT_DATE}.${N4JS_LIBS_COMMIT_ID_LOCAL}" 
+    echo "PUBLISH_VERSION=${PUBLISH_VERSION}"
+
 	echo_exec lerna publish --loglevel silly --skip-git --registry="${NPM_REGISTRY}" --repo-version="${PUBLISH_VERSION}" --since=master --exact --yes --bail --npm-tag="${NPM_TAG}"
 else
 	# Use a version that we are sure can not exist on public npm registry for 
