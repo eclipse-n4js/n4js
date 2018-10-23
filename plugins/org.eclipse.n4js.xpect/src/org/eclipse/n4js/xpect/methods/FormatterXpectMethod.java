8066f1e6b84a984faaee9bcf1b6e2e1306f5e636
mmews <marcus.mews@numberfour.eu>
2018-10-23 15:46:27 +0200

    add/adjust test

--- plugins/org.eclipse.n4js.xpect/src/org/eclipse/n4js/xpect/methods/FormatterXpectMethod.java
+++ plugins/org.eclipse.n4js.xpect/src/org/eclipse/n4js/xpect/methods/FormatterXpectMethod.java
@@ -12,6 +12,23 @@ package org.eclipse.n4js.xpect.methods;
 
 import java.util.List;
 
+import org.eclipse.n4js.formatting2.N4JSFormatterPreferenceKeys;
+import org.eclipse.n4js.xpect.config.Preference;
+import org.eclipse.n4js.xpect.config.Preferences;
+import org.eclipse.n4js.xpect.methods.FormatterXpectMethod.TextRegionAccessFactory;
+import org.eclipse.xpect.XpectImport;
+import org.eclipse.xpect.XpectInvocation;
+import org.eclipse.xpect.expectation.IStringExpectation;
+import org.eclipse.xpect.expectation.StringExpectation;
+import org.eclipse.xpect.expectation.impl.TargetSyntaxSupport;
+import org.eclipse.xpect.parameter.IStatementRelatedRegion;
+import org.eclipse.xpect.parameter.ParameterParser;
+import org.eclipse.xpect.runner.Xpect;
+import org.eclipse.xpect.setup.ISetupInitializer;
+import org.eclipse.xpect.setup.XpectSetupFactory;
+import org.eclipse.xpect.state.Creates;
+import org.eclipse.xpect.xtext.lib.setup.ThisResource;
+import org.eclipse.xpect.xtext.lib.util.XtextTargetSyntaxSupport;
 import org.eclipse.xtext.formatting2.FormatterPreferenceKeys;
 import org.eclipse.xtext.formatting2.FormatterRequest;
 import org.eclipse.xtext.formatting2.IFormatter2;
@@ -20,27 +37,13 @@ import org.eclipse.xtext.formatting2.regionaccess.ITextRegionAccess;
 import org.eclipse.xtext.formatting2.regionaccess.ITextReplacement;
 import org.eclipse.xtext.formatting2.regionaccess.ITextSegment;
 import org.eclipse.xtext.formatting2.regionaccess.TextRegionAccessBuilder;
+import org.eclipse.xtext.nodemodel.ILeafNode;
+import org.eclipse.xtext.nodemodel.util.NodeModelUtils;
 import org.eclipse.xtext.resource.XtextResource;
 import org.eclipse.xtext.util.ExceptionAcceptor;
-import org.eclipse.xpect.XpectImport;
-import org.eclipse.xpect.XpectInvocation;
-import org.eclipse.xpect.expectation.IStringExpectation;
-import org.eclipse.xpect.expectation.StringExpectation;
-import org.eclipse.xpect.expectation.impl.TargetSyntaxSupport;
-import org.eclipse.xpect.parameter.ParameterParser;
-import org.eclipse.xpect.runner.Xpect;
-import org.eclipse.xpect.setup.ISetupInitializer;
-import org.eclipse.xpect.setup.XpectSetupFactory;
-import org.eclipse.xpect.state.Creates;
-import org.eclipse.xpect.xtext.lib.setup.ThisResource;
 
 import com.google.inject.Inject;
 import com.google.inject.Provider;
-
-import org.eclipse.n4js.formatting2.N4JSFormatterPreferenceKeys;
-import org.eclipse.n4js.xpect.config.Preference;
-import org.eclipse.n4js.xpect.config.Preferences;
-import org.eclipse.n4js.xpect.methods.FormatterXpectMethod.TextRegionAccessFactory;
 
 /**
  *
@@ -86,7 +89,15 @@ public class FormatterXpectMethod {
 			TargetSyntaxSupport syntax,
 			ITextRegionAccess reg,
 			ISetupInitializer<Preferences> prefInit) {
-		ITextSegment region = getRegionForLines(reg, syntax.findFirstSemanticCharAfterStatement(inv), lines);
+
+		XtextResource resource = ((XtextTargetSyntaxSupport) syntax).getResource();
+
+		IStatementRelatedRegion region2 = inv.getExtendedRegion();
+		int end = region2.getOffset() + region2.getLength();
+		ILeafNode node = NodeModelUtils.findLeafNodeAtOffset(resource.getParseResult().getRootNode(), end);
+
+		int offset = node.getTotalEndOffset();
+		ITextSegment region = getRegionForLines(reg, offset, lines);
 
 		Preferences prefs = new Preferences();
 		// First put some defaults
