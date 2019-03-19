a4e735d405a73c144038ac8d8331b10f12156f0d
Quang Tran <quang.tran@numberfour.eu>
2019-03-19 10:36:36 +0100

    Move rename refactoring Xpects to testdata_ui folder

--- tests/org.eclipse.n4js.xpect.ui.tests/src/org/eclipse/n4js/xpect/ui/tests/N4JSNotValidatingXpectPluginUITest.java
+++ tests/org.eclipse.n4js.xpect.ui.tests/src/org/eclipse/n4js/xpect/ui/tests/N4JSNotValidatingXpectPluginUITest.java
@@ -17,7 +17,6 @@ import org.eclipse.n4js.xpect.ui.methods.HyperlinkXpectMethod;
 import org.eclipse.n4js.xpect.ui.methods.ProposalXpectMethod;
 import org.eclipse.n4js.xpect.ui.methods.contentassist.ContentAssistXpectMethod;
 import org.eclipse.n4js.xpect.ui.methods.quickfix.QuickFixXpectMethod;
-import org.eclipse.n4js.xpect.ui.refactoring.RenameRefactoringXpectMethod;
 import org.eclipse.n4js.xpect.validation.suppression.N4JSSuppressIssuesSetup;
 import org.eclipse.xpect.XpectImport;
 import org.eclipse.xpect.lib.XpectTestResultTest;
@@ -32,7 +31,7 @@ import org.junit.runner.RunWith;
  * Plugin for proposal test. This plugin turns <b>off</b> validation in xpect-tests by default since most input files
  * are invalid before applying and only some become valid after applying a proposal.
  */
-@XpectSuiteClasses({ 
+@XpectSuiteClasses({
 		// LinkingTest.class,
 		// N4JSTypeSystemXpectTestFragment.class,
 		// NoerrorsValidationTestFragment.class,
@@ -43,8 +42,7 @@ import org.junit.runner.RunWith;
 		ProposalXpectMethod.class,
 		HyperlinkXpectMethod.class,
 		ContentAssistXpectMethod.class,
-		XpectTestResultTest.class,
-		RenameRefactoringXpectMethod.class
+		XpectTestResultTest.class
 })
 @XpectImport({ Config.class, VarDef.class, XpEnvironmentData.class, N4JSSuppressIssuesSetup.class })
 @RunWith(XpectRunner.class)
