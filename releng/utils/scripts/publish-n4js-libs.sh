3d08ef695821f7544542c75153579dde3c45ef23
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2019-03-20 13:43:38 +0100

    adjust 'publish-n4js-libs.sh' for publishing with dist-tag 'next'

--- releng/utils/scripts/publish-n4js-libs.sh
+++ releng/utils/scripts/publish-n4js-libs.sh
@@ -53,7 +53,7 @@ else
 	export NPM_REGISTRY=$2
 fi
 
-# If publishing to local or staging, use the dist-tag 'test' otherwise 'latest'
+# If publishing to local or staging, use the dist-tag 'test' otherwise 'next'
 export NPM_TAG=test
 
 if [ "$DESTINATION" = "public" ]; then
@@ -61,7 +61,7 @@ if [ "$DESTINATION" = "public" ]; then
 		echo "Publishing to public requires the environment variable NPM_TOKEN to be set but it has not been set!"
 		exit 0;
 	fi
-	export NPM_TAG=latest
+	export NPM_TAG=next
 else
 	# Dummy token
 	export NPM_TOKEN=dummy	
@@ -83,30 +83,12 @@ lerna run test
 export NPM_CONFIG_GLOBALCONFIG="$N4JS_LIBS_ROOT"
 echo "Publishing using .npmrc configuration to ${NPM_REGISTRY}";
 
-if [ "${NPM_TAG}" = "latest" ]; then
+if [ "$DESTINATION" = "public" ]; then
     echo "==== STEP 4/10: Checking whether publication of n4js-libs is required (using ${N4JS_LIBS_REPRESENTATIVE} as representative) ..."
-
-    # Obtain the latest commit on npm registry, using the representative
-    N4JS_LIBS_VERSION_PUBLIC=`curl -s ${NPM_REGISTRY}/${N4JS_LIBS_REPRESENTATIVE} | jq -r '.["dist-tags"].latest'`
-    echo "  - version of latest published n4js-libs       : $N4JS_LIBS_VERSION_PUBLIC"
-
-    N4JS_LIBS_COMMIT_ID_PUBLIC=`curl -s ${NPM_REGISTRY}/${N4JS_LIBS_REPRESENTATIVE} | jq -r '.versions."'${N4JS_LIBS_VERSION_PUBLIC}'".gitHead'`
-    if [ "${N4JS_LIBS_COMMIT_ID_PUBLIC}" = "null" ]; then
-        N4JS_LIBS_COMMIT_ID_PUBLIC=unknown
-    fi
-    echo "  - commit ID of latest published n4js-libs     : $N4JS_LIBS_COMMIT_ID_PUBLIC"
+    echo "!!! SKIPPED !!! (will always publish)"
 
     N4JS_LIBS_COMMIT_ID_LOCAL=`git log -1 --format="%H" ${N4JS_LIBS_ROOT}`
     echo "  - commit ID of local n4js-libs                : $N4JS_LIBS_COMMIT_ID_LOCAL"
-
-    # Stop if the local ID equals public ID
-    if [ "${N4JS_LIBS_COMMIT_ID_LOCAL}" = "${N4JS_LIBS_COMMIT_ID_PUBLIC}" ]; then
-        echo '-> will NOT publish (because commit IDs are identical, i.e. no changes since last publication)'
-        echo "==== DONE (nothing published)"
-        exit 0
-    else
-        echo '-> will publish (because commit IDs are different, i.e. there are changes since last publication)'
-    fi
 
     # update repository meta-info in package.json of all n4js-libs to point to the GitHub and the correct commit
     # (yarn isn't doing this at the moment: https://github.com/yarnpkg/yarn/issues/2978 )
@@ -116,35 +98,9 @@ if [ "${NPM_TAG}" = "latest" ]; then
     lerna exec -- rm package.json_TEMP
 
     echo "==== STEP 6/10: Compute new version number for publishing ..."
-    VERSION_MAJOR_REQUESTED=`jq -r '.major' version.json`
-    VERSION_MINOR_REQUESTED=`jq -r '.minor' version.json`
-    echo "Requested major segment       : ${VERSION_MAJOR_REQUESTED} (from file n4js-libs/version.json)"
-    echo "Requested minor segment       : ${VERSION_MINOR_REQUESTED} (from file n4js-libs/version.json)"
-    echo "Latest published version      : ${N4JS_LIBS_VERSION_PUBLIC}"
-    MAJOR_MINOR_PATTERN="([0-9]+)\\.([0-9]+)\\..*"
-    if [[ "${N4JS_LIBS_VERSION_PUBLIC}" =~ ${MAJOR_MINOR_PATTERN} ]]; then
-        VERSION_MAJOR_PUBLIC="${BASH_REMATCH[1]}"
-        VERSION_MINOR_PUBLIC="${BASH_REMATCH[2]}"
-    else
-        echo "ERROR: cannot extract major/minor segments from latest published version!"
-        exit -1
-    fi
-    if [ "$VERSION_MAJOR_REQUESTED" -gt "$VERSION_MAJOR_PUBLIC" ]; then
-        echo "New major version segment requested in file n4js-libs/version.json -> will bump major segment"
-        PUBLISH_VERSION="$VERSION_MAJOR_REQUESTED.$VERSION_MINOR_REQUESTED.0"
-    elif [ "$VERSION_MINOR_REQUESTED" -gt "$VERSION_MINOR_PUBLIC" ]; then
-        echo "New minor version segment requested in file n4js-libs/version.json -> will bump minor segment"
-        # for major segment we use the latest public version as template to make sure
-        # we do not end up with a lower version than the latest public version
-        PUBLISH_VERSION="$VERSION_MAJOR_PUBLIC.$VERSION_MINOR_REQUESTED.0"
-    elif [ "$VERSION_MAJOR_REQUESTED" -eq "$VERSION_MAJOR_PUBLIC" ] && [ "$VERSION_MINOR_REQUESTED" -eq "$VERSION_MINOR_PUBLIC" ]; then
-        echo "No new major/minor version segment requested in file n4js-libs/version.json -> will bump patch segment"
-        PUBLISH_VERSION=`semver -i patch ${N4JS_LIBS_VERSION_PUBLIC}`
-    else
-        echo "ERROR: requested major/minor segment must not be lower than latest published major/minor segment!"
-        exit -1
-    fi
-    echo "Bumped version for publishing : ${PUBLISH_VERSION}"
+    echo "!!! SKIPPED !!! (using version hard-coded in shell script)"
+    export PUBLISH_VERSION="0.13.6-next.20190320"
+    echo "Version for publishing: ${PUBLISH_VERSION}"
 
     echo "==== STEP 7/10: Appending version information to README.md files ..."
     export VERSION_INFO="\n\n## Version\n\nVersion ${PUBLISH_VERSION} of \${LERNA_PACKAGE_NAME} was built from commit [${N4JS_LIBS_COMMIT_ID_LOCAL}](https://github.com/eclipse/n4js/tree/${N4JS_LIBS_COMMIT_ID_LOCAL}/n4js-libs/packages/\${LERNA_PACKAGE_NAME}).\n\n"
@@ -161,7 +117,7 @@ lerna exec -- cp package.json package.json_TEMP
 lerna exec -- 'jq -r ".repository |= {type: \"git\", url: \"https://github.com/eclipse/n4js/tree/master/n4js-libs/packages/$LERNA_PACKAGE_NAME\"}" package.json_TEMP > package.json'
 lerna exec -- rm package.json_TEMP
 
-echo "==== STEP 9/10: Now publishing with version: ${PUBLISH_VERSION}"
+echo "==== STEP 9/10: Now publishing with version: ${PUBLISH_VERSION} and dist-tag ${NPM_TAG}"
 lerna publish --loglevel info --skip-git --registry="${NPM_REGISTRY}" --repo-version="${PUBLISH_VERSION}" --exact --yes --bail --npm-tag="${NPM_TAG}"
 
 echo "==== STEP 10/10: Remove node_modules after publishing"
