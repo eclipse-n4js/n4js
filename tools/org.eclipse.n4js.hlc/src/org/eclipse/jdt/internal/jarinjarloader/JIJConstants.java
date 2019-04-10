6c3de2386527f0b58d02213fc30a04bab750a524
Quang Tran <quang.tran@numberfour.eu>
2019-04-10 15:24:48 +0200

    Update JarRsrcLoader that can handle Java 9 modules

--- tools/org.eclipse.n4js.hlc/src/org/eclipse/jdt/internal/jarinjarloader/JIJConstants.java
+++ tools/org.eclipse.n4js.hlc/src/org/eclipse/jdt/internal/jarinjarloader/JIJConstants.java
@@ -1,9 +1,12 @@
 /*******************************************************************************
- * Copyright (c) 2007, 2009 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ * Copyright (c) 2007, 2017 IBM Corporation and others.
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     Ferenc Hechler, ferenc_hechler@users.sourceforge.net - 262748 [jar exporter] extract constants for string literals in JarRsrcLoader et al.
@@ -14,11 +17,10 @@ package org.eclipse.jdt.internal.jarinjarloader;
  * Constants used in the Jar-in-Jar loader.
  *
  * Some of these are duplicated in JIJConstants in the source for the Runnable Jar File Export Wizard:
- * {@link org.eclipse.jdt.internal.ui.jarpackagerfat}.
+ * org.eclipse.jdt.internal.ui.jarpackagerfat.
  *
  * @since 3.6
  */
-@SuppressWarnings("javadoc")
 final class JIJConstants {
 
 	static final String REDIRECTED_CLASS_PATH_MANIFEST_NAME = "Rsrc-Class-Path"; //$NON-NLS-1$
@@ -32,4 +34,5 @@ final class JIJConstants {
 	static final String PATH_SEPARATOR = "/"; //$NON-NLS-1$
 	static final String CURRENT_DIR = "./"; //$NON-NLS-1$
 	static final String UTF8_ENCODING = "UTF-8"; //$NON-NLS-1$
+	static final String RUNTIME = "#runtime"; //$NON-NLS-1$
 }
--- tools/org.eclipse.n4js.hlc/src/org/eclipse/jdt/internal/jarinjarloader/JarRsrcLoader.java
+++ tools/org.eclipse.n4js.hlc/src/org/eclipse/jdt/internal/jarinjarloader/JarRsrcLoader.java
@@ -1,9 +1,12 @@
 /*******************************************************************************
  * Copyright (c) 2008, 2009 IBM Corporation and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
+ *
+ * This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License 2.0
  * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
+ * https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
  *
  * Contributors:
  *     Ferenc Hechler - initial API and implementation
@@ -30,6 +33,8 @@ import java.util.jar.Manifest;
  * This class will be compiled into the binary jar-in-jar-loader.zip. This ZIP is used for the "Runnable JAR File
  * Exporter"
  *
+ * Copied from https://git.eclipse.org/c/jdt/eclipse.jdt.ui.git/plain/org.eclipse.jdt.ui/jar%20in%20jar%20loader/
+ *
  * @since 3.5
  */
 @SuppressWarnings({ "javadoc", "resource" })
@@ -54,11 +59,26 @@ public class JarRsrcLoader {
 				rsrcUrls[i] = new URL(JIJConstants.JAR_INTERNAL_URL_PROTOCOL_WITH_COLON + rsrcPath
 						+ JIJConstants.JAR_INTERNAL_SEPARATOR);
 		}
-		ClassLoader jceClassLoader = new URLClassLoader(rsrcUrls, null);
+		ClassLoader jceClassLoader = new URLClassLoader(rsrcUrls, getParentClassLoader());
 		Thread.currentThread().setContextClassLoader(jceClassLoader);
 		Class<?> c = Class.forName(mi.rsrcMainClass, true, jceClassLoader);
 		Method main = c.getMethod(JIJConstants.MAIN_METHOD_NAME, new Class[] { args.getClass() });
 		main.invoke((Object) null, new Object[] { args });
+	}
+
+	private static ClassLoader getParentClassLoader() throws InvocationTargetException, IllegalAccessException {
+		// On Java8, it is ok to use a null parent class loader, but, starting with Java 9,
+		// we need to provide one that has access to the restricted list of packages that
+		// otherwise would produce a SecurityException when loaded
+		try {
+			// We use reflection here because the method ClassLoader.getPlatformClassLoader()
+			// is only present starting from Java 9
+			Method platformClassLoader = ClassLoader.class.getMethod("getPlatformClassLoader", (Class[]) null); //$NON-NLS-1$
+			return (ClassLoader) platformClassLoader.invoke(null, (Object[]) null);
+		} catch (NoSuchMethodException e) {
+			// This is a safe value to be used on Java 8 and previous versions
+			return null;
+		}
 	}
 
 	private static ManifestInfo getManifestInfo() throws IOException {
