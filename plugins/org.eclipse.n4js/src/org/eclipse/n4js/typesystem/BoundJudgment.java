f049d364b95244d5ac6240f144c2c36b0b55e0f6
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2019-08-21 07:55:41 +0200

    improve BoundJudgment (part 1)

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/typesystem/BoundJudgment.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/typesystem/BoundJudgment.java
@@ -13,8 +13,11 @@ package org.eclipse.n4js.typesystem;
 import static org.eclipse.n4js.typesystem.utils.RuleEnvironmentExtensions.bottomTypeRef;
 import static org.eclipse.n4js.typesystem.utils.RuleEnvironmentExtensions.topTypeRef;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.stream.Collectors;
 
+import org.eclipse.emf.ecore.EObject;
 import org.eclipse.n4js.ts.typeRefs.BoundThisTypeRef;
 import org.eclipse.n4js.ts.typeRefs.ExistentialTypeRef;
 import org.eclipse.n4js.ts.typeRefs.FunctionTypeExprOrRef;
@@ -23,6 +26,7 @@ import org.eclipse.n4js.ts.typeRefs.IntersectionTypeExpression;
 import org.eclipse.n4js.ts.typeRefs.ParameterizedTypeRef;
 import org.eclipse.n4js.ts.typeRefs.TypeArgument;
 import org.eclipse.n4js.ts.typeRefs.TypeRef;
+import org.eclipse.n4js.ts.typeRefs.TypeRefsPackage;
 import org.eclipse.n4js.ts.typeRefs.TypeTypeRef;
 import org.eclipse.n4js.ts.typeRefs.UnionTypeExpression;
 import org.eclipse.n4js.ts.typeRefs.Wildcard;
@@ -31,6 +35,9 @@ import org.eclipse.n4js.ts.types.TypeVariable;
 import org.eclipse.n4js.ts.utils.TypeUtils;
 import org.eclipse.n4js.typesystem.utils.BoundType;
 import org.eclipse.n4js.typesystem.utils.RuleEnvironment;
+import org.eclipse.n4js.utils.RecursionGuard;
+
+import com.google.common.base.Optional;
 
 /* package */ class BoundJudgment extends AbstractJudgment {
 
@@ -61,9 +68,25 @@ import org.eclipse.n4js.typesystem.utils.RuleEnvironment;
 		private final RuleEnvironment G;
 		private final BoundType boundType;
 
+		private final RecursionGuard<EObject> guard = new RecursionGuard<>();
+
 		public BoundSwitch(RuleEnvironment G, BoundType boundType) {
 			this.G = G;
 			this.boundType = boundType;
+		}
+
+		@Override
+		protected TypeRef doSwitch(int classifierID, EObject eObject) {
+			if (guard.tryNext(eObject)) {
+				try {
+					return super.doSwitch(classifierID, eObject);
+				} finally {
+					guard.done(eObject);
+				}
+			}
+			return eObject instanceof Wildcard
+					? getFallbackBoundForWildcard((Wildcard) eObject)
+					: (TypeRef) eObject;
 		}
 
 		@Override
@@ -79,6 +102,10 @@ import org.eclipse.n4js.typesystem.utils.RuleEnvironment;
 			if (declBound != null) {
 				return declBound;
 			}
+			return getFallbackBoundForWildcard(wildcard);
+		}
+
+		private TypeRef getFallbackBoundForWildcard(@SuppressWarnings("unused") Wildcard wildcard) {
 			return boundType == BoundType.UPPER ? topTypeRef(G) : bottomTypeRef(G);
 		}
 
@@ -112,8 +139,56 @@ import org.eclipse.n4js.typesystem.utils.RuleEnvironment;
 			if (ptr.getDeclaredType() instanceof TypeVariable) {
 				return ptr; // do not return the declaredLowerBounds here! (a type variable is not an existential type)
 			} else {
+				final List<TypeArgument> typeArgs = ptr.getTypeArgs();
+				final Optional<List<TypeArgument>> typeArgsBounds = pushBoundsOfTypeArguments(typeArgs);
+				if (typeArgsBounds.isPresent()) {
+					final ParameterizedTypeRef ptrCpy = TypeUtils.copyPartial(ptr,
+							TypeRefsPackage.eINSTANCE.getParameterizedTypeRef_TypeArgs());
+					ptrCpy.getTypeArgs().clear();
+					ptrCpy.getTypeArgs().addAll(TypeUtils.copyAll(typeArgsBounds.get()));
+					return ptrCpy;
+				}
 				return ptr;
 			}
+		}
+
+		private Optional<List<TypeArgument>> pushBoundsOfTypeArguments(List<TypeArgument> typeArgs) {
+			final List<TypeArgument> typeArgsBounds = new ArrayList<>(typeArgs.size());
+			boolean haveChange = false;
+			for (TypeArgument typeArg : typeArgs) {
+				final TypeArgument typeArgBound = pushBoundOfTypeArgument(typeArg);
+				typeArgsBounds.add(typeArgBound);
+				haveChange |= typeArgBound != typeArg;
+			}
+			return haveChange ? Optional.of(typeArgsBounds) : Optional.absent();
+		}
+
+		// TODO add support for def-site variance
+		private TypeArgument pushBoundOfTypeArgument(TypeArgument typeArg) {
+			if (typeArg instanceof Wildcard) {
+				final Wildcard wildcard = (Wildcard) typeArg;
+				final TypeRef declBound = boundType == BoundType.UPPER
+						? wildcard.getDeclaredOrImplicitUpperBound()
+						: wildcard.getDeclaredLowerBound();
+				if (declBound != null) {
+					TypeRef boundOfDeclBound = doSwitch(declBound);
+					if (boundOfDeclBound != declBound) {
+						final Wildcard wildcardCpy = TypeUtils.copy(wildcard);
+						if (boundType == BoundType.UPPER) {
+							wildcardCpy.setDeclaredUpperBound(TypeUtils.copyIfContained(boundOfDeclBound));
+						} else {
+							wildcardCpy.setDeclaredLowerBound(TypeUtils.copyIfContained(boundOfDeclBound));
+						}
+						return wildcardCpy;
+					}
+				}
+			} else if (typeArg instanceof ExistentialTypeRef) {
+				final ExistentialTypeRef etr = (ExistentialTypeRef) typeArg;
+				final Wildcard wildcard = etr.getWildcard();
+				final Wildcard wildcardPushed = (Wildcard) pushBoundOfTypeArgument(wildcard);
+				return wildcardPushed;
+			}
+			return typeArg; // no change
 		}
 
 		// required due to multiple inheritance
@@ -130,7 +205,7 @@ import org.eclipse.n4js.typesystem.utils.RuleEnvironment;
 		@Override
 		public TypeRef caseBoundThisTypeRef(BoundThisTypeRef boundThisTypeRef) {
 			if (boundType == BoundType.UPPER) {
-				return TypeUtils.createResolvedThisTypeRef(boundThisTypeRef);
+				return doSwitch(TypeUtils.createResolvedThisTypeRef(boundThisTypeRef));
 			} else {
 				final TypeRef result = bottomTypeRef(G);
 				TypeUtils.copyTypeModifiers(result, boundThisTypeRef);
