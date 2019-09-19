5b0a2ca4b0d96fdb5158dc6667a27f9df34490c8
mmews <marcus.mews@numberfour.eu>
2019-09-19 10:16:20 +0200

    initial import of cli.tests bundle

---
+++ tests/org.eclipse.n4js.cli.tests/src/org/eclipse/n4js/cli/tests/CliTest.java
@@ -0,0 +1,26 @@
+/**
+ * Copyright (c) 2017 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.cli.tests;
+
+import static org.eclipse.n4js.cli.N4jscMain.main;
+
+import org.junit.Test;
+
+/**  */
+public class CliTest {
+
+	/**  */
+	@Test
+	public void testA() {
+		String args[] = {};
+		main(args);
+	}
+}
