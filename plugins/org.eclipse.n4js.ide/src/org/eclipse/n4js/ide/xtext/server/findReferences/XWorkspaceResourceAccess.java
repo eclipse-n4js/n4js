64cf24a3fc6935553d3e53db1eb2d809a78de765
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2019-10-16 18:03:52 +0200

    First round of migration to Java

---
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/xtext/server/findReferences/XWorkspaceResourceAccess.java
@@ -0,0 +1,51 @@
+/**
+ * Copyright (c) 2019 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.ide.xtext.server.findReferences;
+
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.resource.ResourceSet;
+import org.eclipse.n4js.ide.xtext.server.XWorkspaceManager;
+import org.eclipse.xtext.findReferences.IReferenceFinder;
+import org.eclipse.xtext.util.Exceptions;
+import org.eclipse.xtext.util.concurrent.IUnitOfWork;
+
+/**
+ * @author kosyakov - Initial contribution and API
+ * @since 2.11
+ */
+@SuppressWarnings("restriction")
+public class XWorkspaceResourceAccess implements IReferenceFinder.IResourceAccess {
+
+	private final XWorkspaceManager workspaceManager;
+
+	/**
+	 * @param workspaceManager
+	 *            the workspace manager
+	 */
+	public XWorkspaceResourceAccess(XWorkspaceManager workspaceManager) {
+		this.workspaceManager = workspaceManager;
+	}
+
+	@Override
+	public <R> R readOnly(URI targetURI, IUnitOfWork<R, ResourceSet> work) {
+		return this.workspaceManager.doRead(targetURI, (document, resource) -> {
+			if (resource == null) {
+				return null;
+			}
+			try {
+				return work.exec(resource.getResourceSet());
+			} catch (Exception e) {
+				return Exceptions.throwUncheckedException(e);
+			}
+		});
+	}
+
+}
