e7e3a1d923681f3beb20687bea76ff14a07841b5
mmews <marcus.mews@numberfour.eu>
2019-11-13 11:08:32 +0100

    fix getAllResources to exclude files from node_modules folders

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/internal/lsp/N4JSSourceFolder.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/internal/lsp/N4JSSourceFolder.java
@@ -10,9 +10,15 @@
  */
 package org.eclipse.n4js.internal.lsp;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.eclipse.emf.common.util.URI;
+import org.eclipse.n4js.N4JSGlobals;
 import org.eclipse.n4js.projectModel.IN4JSSourceContainer;
 import org.eclipse.n4js.projectModel.lsp.IN4JSSourceFolder;
+import org.eclipse.xtext.util.IAcceptor;
+import org.eclipse.xtext.util.IFileSystemScanner;
 
 /**
  * Wrapper around {@link IN4JSSourceContainer}.
@@ -43,6 +49,25 @@ public class N4JSSourceFolder implements IN4JSSourceFolder {
 	@Override
 	public URI getPath() {
 		return delegate.getLocation().withTrailingPathDelimiter().toURI();
+	}
+
+	/**
+	 * Excludes all files in the folder {@link N4JSGlobals#NODE_MODULES}
+	 *
+	 * @return a list of all URIs that are passed to the acceptor of {@link IFileSystemScanner#scan(URI, IAcceptor)}
+	 */
+	@Override
+	public List<URI> getAllResources(IFileSystemScanner scanner) {
+		List<URI> uris = new ArrayList<>();
+		URI projectBase = getPath();
+		scanner.scan(projectBase, (uri) -> {
+			URI relativeUri = uri.deresolve(projectBase);
+			if (relativeUri.segmentCount() > 0 && !relativeUri.segment(0).equals(N4JSGlobals.NODE_MODULES)) {
+				uris.add(uri);
+			}
+		});
+
+		return uris;
 	}
 
 }
