1d6bff2420086904a0fb8bbd0c184a11ef881f15
philip231001@googlemail.com <philip231001@googlemail.com>
2019-11-25 19:59:09 +0100

    Added content assist tests for overriding methods.
    Signed-off-by: philip231001@googlemail.com <philip231001@googlemail.com>
    (cherry picked from commit 26220e8c86f8986f0ffe094b915727527c0eedbc)

--- tests/org.eclipse.n4js.xpect.ui.tests/src/org/eclipse/n4js/xpect/ui/tests/OverridingContentAssistXpectPluginUITest.java
+++ tests/org.eclipse.n4js.xpect.ui.tests/src/org/eclipse/n4js/xpect/ui/tests/OverridingContentAssistXpectPluginUITest.java
@@ -11,14 +11,35 @@
 package org.eclipse.n4js.xpect.ui.tests;
 
 import org.eclipse.n4js.xpect.common.N4JSXpectRunner;
+import org.eclipse.n4js.xpect.config.Config;
+import org.eclipse.n4js.xpect.config.VarDef;
+import org.eclipse.n4js.xpect.config.XpEnvironmentData;
+import org.eclipse.n4js.xpect.ui.methods.HyperlinkXpectMethod;
+import org.eclipse.n4js.xpect.ui.methods.ProposalXpectMethod;
+import org.eclipse.n4js.xpect.ui.methods.contentassist.ContentAssistXpectMethod;
+import org.eclipse.n4js.xpect.ui.methods.quickfix.QuickFixXpectMethod;
+import org.eclipse.n4js.xpect.validation.suppression.N4JSSuppressIssuesSetup;
+import org.eclipse.xpect.XpectImport;
+import org.eclipse.xpect.lib.XpectTestResultTest;
+import org.eclipse.xpect.runner.XpectSuiteClasses;
 import org.eclipse.xpect.runner.XpectTestFiles;
+import org.eclipse.xpect.xtext.lib.tests.ValidationTest;
 import org.junit.runner.RunWith;
 
 /**
  *
  */
+@XpectSuiteClasses({
+		ValidationTest.class,
+		QuickFixXpectMethod.class,
+		ProposalXpectMethod.class,
+		HyperlinkXpectMethod.class,
+		ContentAssistXpectMethod.class,
+		XpectTestResultTest.class
+})
+@XpectImport({ Config.class, VarDef.class, XpEnvironmentData.class, N4JSSuppressIssuesSetup.class })
 @RunWith(N4JSXpectRunner.class)
-@XpectTestFiles(baseDir = "testdata_ui/contentassist")
+@XpectTestFiles(baseDir = "testdata_nonvalidating")
 public class OverridingContentAssistXpectPluginUITest {
 	// nothing more required
 }
