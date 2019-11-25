65cfe678b185f3e89390ff8ae5921ede503a7c6b
Max Neuwirt <max_neuwirt@web.de>
2019-11-25 11:32:37 +0100

    added ui test and xpect file
    Signed-off-by: Max Neuwirt <max_neuwirt@web.de>
    (cherry picked from commit 1b4dc963fb306825f5a6c199a1e0ecb786de7b20)

---
+++ tests/org.eclipse.n4js.xpect.ui.tests/src/org/eclipse/n4js/xpect/ui/tests/OverridingContentAssistXpectPluginUITest.java
@@ -0,0 +1,24 @@
+/**
+ * Copyright (c) 2019 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.xpect.ui.tests;
+
+import org.eclipse.n4js.xpect.common.N4JSXpectRunner;
+import org.eclipse.xpect.runner.XpectTestFiles;
+import org.junit.runner.RunWith;
+
+/**
+ *
+ */
+@RunWith(N4JSXpectRunner.class)
+@XpectTestFiles(baseDir = "testdata_ui/contentassist")
+public class OverridingContentAssistXpectPluginUITest {
+	// nothing more required
+}
