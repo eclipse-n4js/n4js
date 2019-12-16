960cab84dc70af4bca5e62538aa86a9f9c70faba
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2019-12-16 16:22:59 +0100

    adjust tests

--- tests/org.eclipse.n4js.transpiler.es5.tests/testdata_external_impls/classes/AT_874_InheritConstructor/AT_874_ExternalClasses.js
+++ tests/org.eclipse.n4js.transpiler.es5.tests/testdata_external_impls/classes/AT_874_InheritConstructor/AT_874_ExternalClasses.js
@@ -9,59 +9,41 @@
  *   NumberFour AG - Initial API and implementation
  */
 
-MyExternalClass = function MyExternalClass(start, spec, end) {
-    if (spec) {
-        this.stuff = spec.stuff;
-    } else {
-        this.stuff = undefined;
-    }
+import 'n4js-runtime'
+
+export class MyExternalClass extends N4Object {
+	constructor(start, spec, end) {
+		super(start, spec, end);
+		const $specObj = spec || {};
+		this.stuff = $specObj.stuff;
+	}
+	static get n4type() {
+		const $sym = Symbol.for('org.eclipse.n4js/reflectionInfo');
+		if (this.hasOwnProperty($sym)) {
+			return this[$sym];
+		}
+		const instanceProto = this.prototype, staticProto = this;
+		return this[$sym] = new N4Class({
+			name: 'MyExternalClass',
+			origin: 'ES6_Classes',
+			fqn: 'tests/Temp/MyExternalClass',
+			n4superType: N4Object.n4type,
+			allImplementedInterfaces: [],
+			ownedMembers: [
+				new N4DataField({
+					name: 'stuff',
+					isStatic: false,
+					annotations: []
+				}),
+				new N4Method({
+					name: 'constructor',
+					isStatic: false,
+					jsFunction: instanceProto['constructor'],
+					annotations: []
+				})
+			],
+			consumedMembers: [],
+			annotations: []
+		});
+	}
 }
-
-$makeClass(MyExternalClass, Object, [], {}, {},
-    function(instanceProto, staticProto) {
-        var metaClass = new N4Class({
-            name: 'MyExternalClass',
-            origin: 'org.eclipse.n4js.transpiler.es5.tests-0.0.1',
-            fqn: 'classes.AT_874_InheritConstructor.AT_874_ExternalClasses.MyExternalClass',
-            n4superType: N4Object.n4type,
-            allImplementedInterfaces: [],
-            annotations: [],
-            ownedMembers: [new N4DataField({
-                name: 'stuff',
-                annotations: []
-            }), new N4Method({
-                name: 'constructor',
-                jsFunction: instanceProto['constructor'],
-                annotations: []
-            }), ],
-            consumedMembers: []
-        });
-        metaClass.annotations.forEach(function(a) {
-            Object.defineProperty(a, 'target', {
-                value: metaClass,
-                enumerable: false
-            });
-        });
-        if (typeof metaClass.ownedMembers !== 'undefined') {
-            metaClass.ownedMembers.forEach(function(m) {
-                Object.defineProperty(m, 'owner', {
-                    value: metaClass,
-                    enumerable: false
-                });
-                if (typeof m.annotations !== 'undefined') {
-                    m.annotations.forEach(function(a) {
-                        Object.defineProperty(a, 'target', {
-                            value: m,
-                            enumerable: false
-                        });
-                    })
-                }
-            });
-        }
-        return metaClass;
-    }
-);
-
-module.exports = {
-	'MyExternalClass': MyExternalClass};
-        
