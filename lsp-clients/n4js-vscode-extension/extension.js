06a400368622cdcc4288614135afde1504ffa7bb
mmews <marcus.mews@numberfour.eu>
2020-01-30 10:00:09 +0100

    several updates in libs and lsp-vscode-extension

--- lsp-clients/n4js-vscode-extension/extension.js
+++ lsp-clients/n4js-vscode-extension/extension.js
@@ -1,5 +1,3 @@
-#!/usr/bin/env node
-
 /**
  * Copyright (c) 2016 NumberFour AG.
  * All rights reserved. This program and the accompanying materials
@@ -10,10 +8,19 @@
  * Contributors:
  *   NumberFour AG - Initial API and implementation
  */
+
 "use strict";
 
+// The following two modules cannot be imported using ES6 imports.
+// Hence this wrapper loads them using require and passes them to the extensionProvider.
+
+const vscode = require("vscode");
+const vscodeLC = require("vscode-languageclient");
 
 require = require("esm")(module);
-let n4jsc = require("../src-gen/n4jsc.js");
 
-n4jsc.runN4jscSync();
+const extensionProvider = require("./src-gen/extensionProvider.js");
+
+module.exports = {
+    activate: extensionProvider.get(vscode, vscodeLC)
+}
