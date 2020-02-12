1c84e65286043c02b7907a5dcb4cc54f455e2006
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2020-02-12 11:33:20 +0100

    temporary: fix reentrant invocation in performance data collection

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/validation/helper/IssuesProvider.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/validation/helper/IssuesProvider.java
@@ -72,7 +72,12 @@ public final class IssuesProvider implements Provider<List<Issue>> {
 				? N4JSDataCollectors.dcValidationsPackageJson
 				: N4JSDataCollectors.dcValidations;
 		List<Issue> issues;
-		try (Measurement m = dc.getMeasurement("validation");) {
+		// FIXME revert this change and find better solution!
+		if (!dc.hasActiveMeasurement()) {
+			try (Measurement m = dc.getMeasurement("validation");) {
+				issues = rv.validate(r, checkMode, ci);
+			}
+		} else {
 			issues = rv.validate(r, checkMode, ci);
 		}
 		if (!issues.contains(null)) {
