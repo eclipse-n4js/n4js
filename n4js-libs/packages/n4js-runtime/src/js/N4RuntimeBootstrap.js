ae2fb39dceea9f0a326d3b9f7b0446e279d745d9
mmews <marcus.mews@numberfour.eu>
2020-03-05 16:34:32 +0100

    first version of minimized reflection

--- n4js-libs/packages/n4js-runtime/src/js/N4RuntimeBootstrap.js
+++ n4js-libs/packages/n4js-runtime/src/js/N4RuntimeBootstrap.js
@@ -54,7 +54,7 @@ function mixinDefaultMethods(iface) {
  * @param staticMethods - An object holding the descriptors for the class static methods
  * @param n4typeFn - Optional factory function to create the meta type (currently mandatory though, will be optional with GH-574).
  */
-function $makeClass(ctor, superCtor, implementedInterfaces, instanceMethods, staticMethods, n4typeFn) {
+function $makeClass_orig(ctor, superCtor, implementedInterfaces, instanceMethods, staticMethods, n4typeFn) {
     if (typeof superCtor === "function") {
         Object.setPrototypeOf(ctor, superCtor);
     }
@@ -72,6 +72,202 @@ function $makeClass(ctor, superCtor, implementedInterfaces, instanceMethods, sta
 
     ctor.prototype = proto;
 }
+/**
+ * Setup a constructor function to work as a class.
+ *
+ * @param ctor - The constructor function
+ * @param superCtor - The constructor function of the super class
+ * @param implementedInterfaces - Array of directly implemented interfaces, excluding built-in types and
+ *                                interfaces defined in definition files without annotation @N4JS.
+ * @param instanceMethods - An object holding the methods for the class instance and mixed in methods
+ * @param staticMethods - An object holding the descriptors for the class static methods
+ * @param n4typeValues - Optional factory function to create the meta type (currently mandatory though, will be optional with GH-574).
+ */
+function $makeClass(ctor, superCtor, implementedInterfaces, instanceMethods, staticMethods, reflectionString) {
+    if (typeof superCtor === "function") {
+        Object.setPrototypeOf(ctor, superCtor);
+    }
+    Object.defineProperties(ctor, staticMethods);
+
+    var proto = Object.create(superCtor.prototype, instanceMethods);
+    implementedInterfaces.forEach(mixinDefaultMethods, proto);
+    Object.defineProperty(proto, "constructor", {
+        value: ctor
+    });
+
+    if (reflectionString) {
+        defineN4TypeGetter(ctor, makeReflectionsForClass.bind(null, proto, ctor, reflectionString));
+    }
+
+    ctor.prototype = proto;
+}
+
+
+/** Used for ES6 class version of N4JS */
+//function $getReflectionForClass(n4elem, moduleName, n4ElemName) {
+//    const $sym = Symbol.for('org.eclipse.n4js/reflectionInfo');
+//    if (n4elem.hasOwnProperty($sym)) {
+//        return n4elem[$sym];
+//    }
+//    // FIXME: path of require is wrong
+//    const reflectValues = require(moduleName + '.reflect')?.[n4ElemName];
+//    if (!reflectValues) {
+//        return null;
+//    }
+//    const instanceProto = n4elem.prototype;
+//    return createN4Class(instanceProto, staticProto, ...reflectValues);
+//}
+
+function makeReflectionsForClass(instanceProto, staticProto, reflectionString) {
+	const reflectionValues = JSON.parse(reflectionString);
+	const superclass = staticProto.__proto__;
+	const n4Class = new N4Class();
+    setN4TypeProperties(n4Class, ...reflectionValues);
+    setN4ClassifierProperties(n4Class, superclass, instanceProto, staticProto, ...reflectionValues);
+    return n4Class;
+}
+
+function makeReflectionsForInterface(instanceProto, staticProto, reflectionString) {
+	const reflectionValues = JSON.parse(reflectionString);
+	const n4Interface = new N4Interface();
+    setN4TypeProperties(n4Interface, ...reflectionValues);
+    setN4ClassifierProperties(n4Interface, undefined, instanceProto, staticProto, ...reflectionValues);
+    return n4Interface;
+}
+
+function makeReflectionsForEnum(reflectionString) {
+	const reflectionValues = JSON.parse(reflectionString);
+	const n4enumType = new N4EnumType();
+    setN4TypeProperties(n4enumType, ...reflectionValues);
+    return n4enumType;
+}
+
+function setN4TypeProperties(n4Type, name, modulePath, origin, members, memberAnnotations, allImplementedInterfaces, annotations) {
+    n4Type.name = name;
+    n4Type.origin = origin;
+    n4Type.annotations = createAnnotations(annotations);
+    n4Type.fqn = modulePath + '/' + name;
+}
+
+function setN4ClassifierProperties(n4Classifier, superclass, instanceProto, staticProto, name, modulePath, origin, members, memberAnnotations, allImplementedInterfaces, annotations) {
+	const splitMembers = createMembers(instanceProto, staticProto, members, memberAnnotations);
+    n4Classifier.superclass = superclass;
+    n4Classifier.ownedMembers = splitMembers.ownedMembers;
+    n4Classifier.consumedMembers = splitMembers.consumedMembers;
+    n4Classifier.allImplementedInterfaces = allImplementedInterfaces || [];
+}
+
+function createMembers(instanceProto, staticProto, memberStrings, memberAnnotations) {
+	const annotations = createMemberAnnotations(memberAnnotations);
+    const ownedMembers = [];
+    const consumedMembers = [];
+	if (memberStrings) {
+        for (const memberString of memberStrings) {
+			const memberInfo = parseMemberInfo(memberString);
+			const member = createMember(instanceProto, staticProto, memberInfo, annotations);
+			const members = (memberInfo.isConsumed) ? consumedMembers : ownedMembers;
+            members.push(member);
+        }		
+	}
+
+    return {ownedMembers, consumedMembers};
+}
+
+function createMemberAnnotations(memberAnnotations) {
+    const annotations = {};
+    if (memberAnnotations) {
+        for (const memberName of Object.keys(memberAnnotations)) {
+            const annotationArray = [];
+            annotations[memberName] = annotationArray;
+            for (const memberAnnotation of memberAnnotations[memberName]) {
+                const annotation = createAnnotation(...memberAnnotation);
+                if (annotation) {
+                    annotationArray.push(annotation);
+                }
+			}
+		}
+	}
+	return annotations;
+}
+
+function createAnnotations(annotations) {
+    const annotationArray = [];
+    if (annotations) {
+        for (const annotationValues of annotations) {
+            const annotation = createAnnotation(annotationValues);
+            if (annotation) {
+                annotationArray.push(annotation);
+            }
+        }
+	}
+	return annotationArray;
+}
+
+function createAnnotation(annotationValues) {
+    const annotation = new N4Annotation();
+	if (typeof annotationValues === "string") {
+    	annotation.name = annotationValues;
+    	annotation.details = [];
+	} else {
+		val [val, details] = annotationValues;
+    	annotation.name = name;
+    	annotation.details = details || [];
+	}
+    return annotation;
+}
+
+function parseMemberInfo(memberString) {
+	if (!/^[mMfFgGsS][\.:]/.test(memberString)) {
+		return null;
+	}
+	const idxKind = 0;
+	const idxConsumed = idxKind + 1;
+	const idxNameStart = idxConsumed + 1;
+	const kind = memberString[idxKind].toLowerCase();	
+    const isUpperCase = (memberString[idxKind] == memberString[idxKind].toUpperCase());
+
+	let isConsumed = undefined;
+	switch (memberString[idxConsumed].toLowerCase()) {
+		case '.': isConsumed = false; break;
+		case ':': isConsumed = true; break;
+		default: isConsumed = undefined;
+	}
+	
+	const name = memberString.substring(idxNameStart);
+
+	return {memberString, name, kind, isStatic: isUpperCase, isConsumed};
+}
+
+function createMember(instanceProto, staticProto, memberInfo, annotations) {
+	var member = null;
+	switch (memberInfo.kind) {
+		case 'f':
+			member = new N4DataField();
+			break;
+		case 'm': 
+			member = new N4Method();
+    		member.jsFunction = memberInfo.isStatic ? staticProto[memberInfo.name] : instanceProto[memberInfo.name];
+			break;
+		case 'g': 
+			member = new N4Accessor();
+    		member.getter = true;
+			break;
+		case 's':
+			member = new N4Accessor();
+    		member.getter = false;
+			break;
+		default:
+			return null;
+	}
+	
+    member.name = memberInfo.name;
+    member.isStatic = memberInfo.isStatic;
+    member.annotations = annotations[memberInfo.memberString] || [];
+	return member;
+}
+
+
+
 
 /**
  * Setup a interface. Methods and field initializers are already merged into the interface object.
@@ -81,11 +277,11 @@ function $makeClass(ctor, superCtor, implementedInterfaces, instanceMethods, sta
  *                               May be 'undefined' in case no interfaces are extended.
  * @param n4typeFn - Optional factory function to create the meta type (currently mandatory though, will be optional with GH-574).
  */
-function $makeInterface(tinterface, extendedInterfacesFn, n4typeFn) {
+function $makeInterface(tinterface, extendedInterfacesFn, reflectionString) {
     tinterface.$extends = extendedInterfacesFn || (()=>[]);
 
-    if (n4typeFn) {
-        defineN4TypeGetter(tinterface, n4typeFn.bind(null, tinterface.$methods, tinterface));
+    if (reflectionString) {
+        defineN4TypeGetter(tinterface, makeReflectionsForInterface.bind(null, tinterface.$methods, tinterface, reflectionString));
     }
 
     Object.defineProperty(tinterface, symHasInstance, {
@@ -104,6 +300,11 @@ function $makeInterface(tinterface, extendedInterfacesFn, n4typeFn) {
         }
     });
 }
+
+
+
+
+
 
 /**
  * Initialize the fields declared by the given interfaces in the target object 'target'.
@@ -149,14 +350,14 @@ function $initFieldsFromInterfaces(target, interfaces, spec, mixinExclusion) {
  * @param n4typeFn - Optional factory function to create the meta type (currently mandatory though, will be optional with GH-574).
  * @return The constructed enumeration type
  */
-function $makeEnum(enumeration, members, n4typeFn) {
+function $makeEnum(enumeration, members, reflectionString) {
     var length, index, member, name, value, values, literal;
 
     Object.setPrototypeOf(enumeration, N4Enum);
     enumeration.prototype = Object.create(N4Enum.prototype, {});
 
-    if (n4typeFn) {
-        defineN4TypeGetter(enumeration, n4typeFn.bind(null, noop));
+    if (reflectionString) {
+        defineN4TypeGetter(enumeration, makeReflectionsForEnum.bind(null, reflectionString));
     }
 
     Object.defineProperty(enumeration.prototype, "constructor", {
