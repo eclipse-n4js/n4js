ad1d0a81e736001d0590b1a1a37d00664c95e4da
mmews <marcus.mews@numberfour.eu>
2020-04-15 12:56:46 +0200

    another try

--- releng/utils/scripts/publish-vscode-extension.sh
+++ releng/utils/scripts/publish-vscode-extension.sh
@@ -68,7 +68,7 @@ else
 fi
 
 
-cat ${N4JS_DIR}
+#cat ${N4JS_DIR}
 cat ${N4JS_DIR}/releng
 cat ${N4JS_DIR}/releng/org.eclipse.n4js.libs.build
 cat ${N4JS_DIR}/releng/org.eclipse.n4js.libs.build/verdaccioConfig
