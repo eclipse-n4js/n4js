572795281a62242c79e17e74156ac6fc4419ae48
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2020-04-22 10:40:56 +0200

    GH-1733: Remove obsolete code

--- plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/editor/contentassist/N4JSContentAssistContextFactory.java
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/editor/contentassist/N4JSContentAssistContextFactory.java
@@ -10,19 +10,11 @@
  */
 package org.eclipse.n4js.ide.editor.contentassist;
 
-import java.util.Collection;
-
-import org.antlr.runtime.ANTLRStringStream;
-import org.antlr.runtime.Token;
-import org.eclipse.emf.ecore.EObject;
 import org.eclipse.n4js.smith.Measurement;
 import org.eclipse.xtext.ide.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ide.editor.contentassist.antlr.ContentAssistContextFactory;
-import org.eclipse.xtext.ide.editor.contentassist.antlr.FollowElement;
-import org.eclipse.xtext.nodemodel.ILeafNode;
-import org.eclipse.xtext.nodemodel.INode;
+import org.eclipse.xtext.ide.editor.contentassist.antlr.PartialContentAssistContextFactory;
 import org.eclipse.xtext.parser.IParseResult;
-import org.eclipse.xtext.util.Strings;
 
 import com.google.inject.Inject;
 
@@ -37,7 +29,7 @@ import com.google.inject.Inject;
  *
  * @see CustomN4JSParser#getFollowElements(IParseResult, int, int, boolean)
  */
-public class N4JSContentAssistContextFactory extends ContentAssistContextFactory {
+public class N4JSContentAssistContextFactory extends PartialContentAssistContextFactory {
 
 	@Inject
 	private ContentAssistDataCollectors dataCollectors;
@@ -46,60 +38,6 @@ public class N4JSContentAssistContextFactory extends ContentAssistContextFactory
 	protected ContentAssistContext[] doCreateContexts(int offset) {
 		try (Measurement m = dataCollectors.dcCreateContexts().getMeasurement()) {
 			return super.doCreateContexts(offset);
-		}
-	}
-
-	@Override
-	public CustomN4JSParser getParser() {
-		return (CustomN4JSParser) super.getParser();
-	}
-
-	@Override
-	protected void createContextsForLastCompleteNode(EObject previousModel, boolean strict) {
-		String currentNodePrefix = getPrefix(currentNode);
-		if (!Strings.isEmpty(currentNodePrefix) && !currentNode.getText().equals(currentNodePrefix)) {
-			lexer.setCharStream(new ANTLRStringStream(currentNodePrefix));
-			Token token = lexer.nextToken();
-			if (token == Token.EOF_TOKEN) { // error case - nothing could be parsed
-				return;
-			}
-			while (token != Token.EOF_TOKEN) {
-				if (isErrorToken(token))
-					return;
-				token = lexer.nextToken();
-			}
-		}
-		String prefix = "";
-		Collection<FollowElement> followElements = getParser().getFollowElements(parseResult, 0, completionOffset,
-				strict);
-		doCreateContexts(lastCompleteNode, currentNode, prefix, previousModel, followElements);
-	}
-
-	@Override
-	protected void handleLastCompleteNodeAsPartOfDatatypeNode() {
-		String prefix = getPrefix(datatypeNode);
-		Collection<FollowElement> followElements = getParser().getFollowElements(parseResult, 0,
-				datatypeNode.getOffset(),
-				true);
-		INode lastCompleteNodeBeforeDatatype = getLastCompleteNodeByOffset(rootNode, datatypeNode.getTotalOffset());
-		doCreateContexts(lastCompleteNodeBeforeDatatype, datatypeNode, prefix, currentModel, followElements);
-	}
-
-	@Override
-	protected void handleLastCompleteNodeIsAtEndOfDatatypeNode() {
-		String prefix = getPrefix(lastCompleteNode);
-		INode previousNode = getLastCompleteNodeByOffset(rootNode, lastCompleteNode.getOffset());
-		EObject previousModel = previousNode.getSemanticElement();
-		INode currentDatatypeNode = getContainingDatatypeRuleNode(currentNode);
-		Collection<FollowElement> followElements = getParser().getFollowElements(parseResult, 0,
-				lastCompleteNode.getOffset(), true);
-		int prevSize = contextBuilders.size();
-		doCreateContexts(previousNode, currentDatatypeNode, prefix, previousModel, followElements);
-
-		if (lastCompleteNode instanceof ILeafNode && lastCompleteNode.getGrammarElement() == null
-				&& contextBuilders.size() != prevSize) {
-			handleLastCompleteNodeHasNoGrammarElement(contextBuilders.subList(prevSize, contextBuilders.size()),
-					previousModel);
 		}
 	}
 
