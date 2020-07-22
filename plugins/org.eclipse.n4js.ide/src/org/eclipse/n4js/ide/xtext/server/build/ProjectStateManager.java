0c4f967ee18f99dc98c3c35c20032fb4acc0fee8
Sebastian Zarnekow <sebastian.zarnekow@gmail.com>
2020-07-22 15:32:52 +0200

    GH-1806: Remove obsolete review comment

--- plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/xtext/server/build/ProjectStateManager.java
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/xtext/server/build/ProjectStateManager.java
@@ -145,11 +145,6 @@ public class ProjectStateManager {
 			setValidationIssues(projectState.validationIssues);
 		}
 
-		/*
-		 * Review feedback:
-		 *
-		 * This should probably be handled by the builder itself? Should we return WorkspaceChanges instead?
-		 */
 		Set<URI> allIndexedUris = getIndex().getAllURIs();
 		for (ISourceFolder srcFolder : projectConfig.getSourceFolders()) {
 			List<URI> allSourceFolderUris = srcFolder.getAllResources(fileSystemScanner);
