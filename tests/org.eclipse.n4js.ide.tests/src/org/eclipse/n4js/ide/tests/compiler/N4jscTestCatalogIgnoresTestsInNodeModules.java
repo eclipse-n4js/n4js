8c21989fbfdff95b0897d507c57af20e784e5f93
mmews <marcus.mews@numberfour.eu>
2020-08-04 15:10:20 +0200

    add test to exclude tests from node_modules

---
+++ tests/org.eclipse.n4js.ide.tests/src/org/eclipse/n4js/ide/tests/compiler/N4jscTestCatalogIgnoresTestsInNodeModules.java
@@ -0,0 +1,63 @@
+/**
+ * Copyright (c) 2016 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.ide.tests.compiler;
+
+import static org.eclipse.n4js.cli.N4jscExitCode.VALIDATION_ERRORS;
+import static org.eclipse.n4js.cli.N4jscTestOptions.COMPILE;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.n4js.cli.helper.AbstractCliCompileTest;
+import org.eclipse.n4js.cli.helper.CliCompileResult;
+import org.eclipse.n4js.utils.io.FileDeleter;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Tests that the test catalog file does not contain tests of packages located inside the node_modules folder.
+ */
+public class N4jscTestCatalogIgnoresTestsInNodeModules extends AbstractCliCompileTest {
+
+	static final boolean DONT_CLEAN = false;
+
+	File workspace;
+	File projectRoot;
+
+	/** Prepare workspace. */
+	@Before
+	public void setupWorkspace() throws IOException {
+		workspace = setupWorkspace("testCatalogIgnoresTestsInNodeModules", false);
+		projectRoot = new File(workspace, "P1").getAbsoluteFile();
+	}
+
+	/** Delete workspace. */
+	@After
+	public void deleteWorkspace() throws IOException {
+		FileDeleter.delete(workspace.toPath(), true);
+	}
+
+	/** Compile and check if test catalog exists. */
+	@Test
+	public void testCompileAndAssertTestCatalog() throws Exception {
+
+		File testCatalog = new File(projectRoot, "testcatalog.json"); // N4JSGlobals.TEST_CATALOG
+		CliCompileResult cliResult = n4jsc(COMPILE(workspace).testCatalog(testCatalog),
+				VALIDATION_ERRORS);
+		assertEquals(cliResult.toString(), 1, cliResult.getTranspiledFilesCount());
+
+		assertFalse("Superfluous test catalog in P1", testCatalog.isFile());
+	}
+
+}
