8b64c42a78d713a748204a5c968ba824c27afa43
mmews <marcus.mews@numberfour.eu>
2020-08-07 11:16:44 +0200

    adjust test script for n4js-libs

--- n4js-libs/packages/n4js-mangelhaft-cli/tests/npm-test.sh
+++ n4js-libs/packages/n4js-mangelhaft-cli/tests/npm-test.sh
@@ -12,14 +12,16 @@
 set -e
 cd `dirname $0`/TestPrj
 
-echo "Create test catalog and move to TestPrj"
+echo "Create test catalogs"
 ../../../n4js-cli/bin/n4jsc.js compile ../../../..
-mv ../../../../testcatalog.json ./testcatalog.json
 
-echo "Run mangelhaft"
-../../bin/n4js-mangelhaft-cli.js \
-    --testCatalog ./testcatalog.json \
-    --xunitReportFile ./build/report.xml \
-    --xunitReportName test-report \
-    --xunitReportPackage TestPrj \
-    $@
+find ../../.. -name "testcatalog.json" -type f | while read -r FILE; do
+	echo ""
+    echo "Run mangelhaft for ${FILE}:"
+	../../bin/n4js-mangelhaft-cli.js \
+	    --testCatalog ${FILE} \
+	    --xunitReportFile ./build/report.xml \
+	    --xunitReportName test-report \
+	    --xunitReportPackage TestPrj \
+	    $@
+done
