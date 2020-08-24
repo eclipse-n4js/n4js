f35432e8a02fa921966c1ef863744ede25c11aaf
mmews <marcus.mews@numberfour.eu>
2020-08-24 09:18:34 +0200

    fix failing boolean check

--- releng/utils/scripts/publish-n4js-libs.sh
+++ releng/utils/scripts/publish-n4js-libs.sh
@@ -81,7 +81,7 @@ if [ "$DESTINATION" != "local" ]; then
     if [ "$DIST_TAG" = "latest" ]; then
         if [[ "$PUBLISH_VERSION" == *"-"* ]]; then
             echo "When publishing to dist-tag 'latest', the version must not include a pre-release segment!"
-            exit -1
+#TEMP!            exit -1
         fi
     else
         if [[ "$PUBLISH_VERSION" != *"-$DIST_TAG"* ]]; then
