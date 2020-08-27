418b216a292e52e24f01a6a1306ffb862c78deb8
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2020-08-27 08:16:47 +0200

    review feedback (remove method #findProjectByNestedLocation())

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/xtext/workspace/WorkspaceConfigSnapshot.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/xtext/workspace/WorkspaceConfigSnapshot.java
@@ -15,6 +15,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Objects;
 
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.xtext.workspace.IWorkspaceConfig;
@@ -34,30 +35,23 @@ public class WorkspaceConfigSnapshot {
 	/** Map from project name to the project's configuration snapshot. */
 	protected final ImmutableBiMap<String, ProjectConfigSnapshot> name2Project;
 	/** Keys are URIs <em>without</em> trailing path separator. */
-	protected final ImmutableMap<URI, ProjectConfigSnapshot> projectPath2Project;
-	/** Keys are URIs <em>without</em> trailing path separator. */
 	protected final ImmutableMap<URI, ProjectConfigSnapshot> sourceFolderPath2Project;
 
 	/** See {@link WorkspaceConfigSnapshot}. */
 	public WorkspaceConfigSnapshot(URI path, Iterable<? extends ProjectConfigSnapshot> projects) {
 		Map<String, ProjectConfigSnapshot> lookupName2Project = new HashMap<>();
-		Map<URI, ProjectConfigSnapshot> lookupProjectPath2Project = new HashMap<>();
 		Map<URI, ProjectConfigSnapshot> lookupSourceFolderPath2Project = new HashMap<>();
-		updateLookupMaps(lookupName2Project, lookupProjectPath2Project, lookupSourceFolderPath2Project,
-				projects, Collections.emptyList());
+		updateLookupMaps(lookupName2Project, lookupSourceFolderPath2Project, projects, Collections.emptyList());
 		this.path = path;
 		this.name2Project = ImmutableBiMap.copyOf(lookupName2Project);
-		this.projectPath2Project = ImmutableMap.copyOf(lookupProjectPath2Project);
 		this.sourceFolderPath2Project = ImmutableMap.copyOf(lookupSourceFolderPath2Project);
 	}
 
 	/** See {@link WorkspaceConfigSnapshot}. */
 	protected WorkspaceConfigSnapshot(URI path, ImmutableBiMap<String, ProjectConfigSnapshot> name2Project,
-			ImmutableMap<URI, ProjectConfigSnapshot> projectPath2Project,
 			ImmutableMap<URI, ProjectConfigSnapshot> sourceFolderPath2Project) {
 		this.path = path;
 		this.name2Project = name2Project;
-		this.projectPath2Project = projectPath2Project;
 		this.sourceFolderPath2Project = sourceFolderPath2Project;
 	}
 
@@ -83,15 +77,6 @@ public class WorkspaceConfigSnapshot {
 	}
 
 	/**
-	 * Finds the project having a path that is a prefix of the given URI.
-	 * <p>
-	 * Note the difference to {@link #findProjectContaining(URI)}!
-	 */
-	public ProjectConfigSnapshot findProjectByNestedLocation(URI nestedLocation) {
-		return findInMapByNestedURI(projectPath2Project, nestedLocation);
-	}
-
-	/**
 	 * Same as {@link IWorkspaceConfig#findProjectContaining(URI)}: finds the project having a source folder that
 	 * {@link SourceFolderSnapshot#contains(URI) actually contains} the given nested URI. Returns <code>null</code> if
 	 * no such project is found. The given URI may point to a file or folder.
@@ -100,33 +85,23 @@ public class WorkspaceConfigSnapshot {
 	 * @see SourceFolderSnapshot#contains(URI)
 	 */
 	public ProjectConfigSnapshot findProjectContaining(URI nestedLocation) {
-		ProjectConfigSnapshot candidate = findInMapByNestedURI(sourceFolderPath2Project, nestedLocation);
-		if (candidate != null) {
-			// in addition to checking the source folder paths, we have to make sure the source folder actually
-			// "contains" the URI as defined by method SourceFolderSnapshot#contains(URI):
-			for (SourceFolderSnapshot sourceFolder : candidate.getSourceFolders()) {
-				if (sourceFolder.contains(nestedLocation)) {
-					return candidate;
-				}
-			}
-		}
-		return null;
-	}
-
-	/**
-	 * Returns the value for the key that is the longest prefix of the given URI or <code>null</code> if none of the
-	 * given map's keys is a prefix of the given URI. Assumes keys to be URIs <em>without</em> a trailing path
-	 * separator.
-	 */
-	protected <T> T findInMapByNestedURI(Map<URI, T> map, URI nestedLocation) {
-		URI currPrefix = trimTrailingPathSeparator(nestedLocation);
+		URI currLoc = trimTrailingPathSeparator(nestedLocation);
 		do {
-			T match = map.get(currPrefix);
+			ProjectConfigSnapshot match = sourceFolderPath2Project.get(currLoc);
 			if (match != null) {
-				return match;
+				// in addition to checking the source folder paths, we have to make sure the source folder actually
+				// "contains" the URI as defined by method SourceFolderSnapshot#contains(URI):
+				for (SourceFolderSnapshot sourceFolder : match.getSourceFolders()) {
+					if (Objects.equals(trimTrailingPathSeparator(sourceFolder.getPath()), currLoc)) {
+						if (sourceFolder.contains(nestedLocation)) {
+							return match;
+						}
+					}
+				}
+				return null;
 			}
-			currPrefix = currPrefix.segmentCount() > 0 ? currPrefix.trimSegments(1) : null;
-		} while (currPrefix != null);
+			currLoc = currLoc.segmentCount() > 0 ? currLoc.trimSegments(1) : null;
+		} while (currLoc != null);
 
 		return null;
 	}
@@ -145,18 +120,15 @@ public class WorkspaceConfigSnapshot {
 			Iterable<String> removedProjects) {
 
 		Map<String, ProjectConfigSnapshot> lookupName2Project = new HashMap<>(name2Project);
-		Map<URI, ProjectConfigSnapshot> lookupProjectPath2Project = new HashMap<>(projectPath2Project);
 		Map<URI, ProjectConfigSnapshot> lookupSourceFolderPath2Project = new HashMap<>(sourceFolderPath2Project);
-		updateLookupMaps(lookupName2Project, lookupProjectPath2Project, lookupSourceFolderPath2Project,
-				changedProjects, removedProjects);
+		updateLookupMaps(lookupName2Project, lookupSourceFolderPath2Project, changedProjects, removedProjects);
 		return new WorkspaceConfigSnapshot(path, ImmutableBiMap.copyOf(lookupName2Project),
-				ImmutableMap.copyOf(lookupProjectPath2Project), ImmutableMap.copyOf(lookupSourceFolderPath2Project));
+				ImmutableMap.copyOf(lookupSourceFolderPath2Project));
 	}
 
 	/** Change the given lookup maps to include the given project changes and removals. */
 	protected void updateLookupMaps(
 			Map<String, ProjectConfigSnapshot> lookupName2Project,
-			Map<URI, ProjectConfigSnapshot> lookupProjectPath2Project,
 			Map<URI, ProjectConfigSnapshot> lookupSourceFolderPath2Project,
 			Iterable<? extends ProjectConfigSnapshot> changedProjects, Iterable<String> removedProjectNames) {
 
@@ -173,12 +145,10 @@ public class WorkspaceConfigSnapshot {
 		for (ProjectConfigSnapshot project : changedProjects) {
 			ProjectConfigSnapshot oldProject = lookupName2Project.put(project.getName(), project);
 			if (oldProject != null) {
-				lookupProjectPath2Project.remove(trimTrailingPathSeparator(oldProject.getPath()));
 				for (SourceFolderSnapshot sourceFolder : oldProject.getSourceFolders()) {
 					lookupSourceFolderPath2Project.remove(trimTrailingPathSeparator(sourceFolder.getPath()));
 				}
 			}
-			lookupProjectPath2Project.put(trimTrailingPathSeparator(project.getPath()), project);
 			for (SourceFolderSnapshot sourceFolder : project.getSourceFolders()) {
 				lookupSourceFolderPath2Project.put(trimTrailingPathSeparator(sourceFolder.getPath()), project);
 			}
@@ -187,7 +157,6 @@ public class WorkspaceConfigSnapshot {
 		// apply updates for removed projects
 		for (ProjectConfigSnapshot removedProject : removedProjects) {
 			lookupName2Project.remove(removedProject.getName());
-			lookupProjectPath2Project.remove(trimTrailingPathSeparator(removedProject.getPath()));
 			for (SourceFolderSnapshot sourceFolder : removedProject.getSourceFolders()) {
 				lookupSourceFolderPath2Project.remove(trimTrailingPathSeparator(sourceFolder.getPath()));
 			}
@@ -204,7 +173,6 @@ public class WorkspaceConfigSnapshot {
 		int result = 1;
 		result = prime * result + ((name2Project == null) ? 0 : name2Project.hashCode());
 		result = prime * result + ((path == null) ? 0 : path.hashCode());
-		result = prime * result + ((projectPath2Project == null) ? 0 : projectPath2Project.hashCode());
 		result = prime * result + ((sourceFolderPath2Project == null) ? 0 : sourceFolderPath2Project.hashCode());
 		return result;
 	}
@@ -227,11 +195,6 @@ public class WorkspaceConfigSnapshot {
 			if (other.path != null)
 				return false;
 		} else if (!path.equals(other.path))
-			return false;
-		if (projectPath2Project == null) {
-			if (other.projectPath2Project != null)
-				return false;
-		} else if (!projectPath2Project.equals(other.projectPath2Project))
 			return false;
 		if (sourceFolderPath2Project == null) {
 			if (other.sourceFolderPath2Project != null)
