a426716fcee182af11ad76bc109ed68acb0a7f29
mmews <marcus.mews@numberfour.eu>
2020-08-31 16:56:22 +0200

    override getSymbols to wrap inside temp context

--- plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/xtext/server/symbol/XDocumentSymbolService.java
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/xtext/server/symbol/XDocumentSymbolService.java
@@ -12,9 +12,11 @@ package org.eclipse.n4js.ide.xtext.server.symbol;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.eclipse.emf.ecore.EObject;
 import org.eclipse.lsp4j.Location;
+import org.eclipse.lsp4j.SymbolInformation;
 import org.eclipse.xtext.findReferences.IReferenceFinder;
 import org.eclipse.xtext.findReferences.IReferenceFinder.IResourceAccess;
 import org.eclipse.xtext.findReferences.ReferenceAcceptor;
@@ -23,10 +25,14 @@ import org.eclipse.xtext.ide.server.DocumentExtensions;
 import org.eclipse.xtext.ide.server.symbol.DocumentSymbolService;
 import org.eclipse.xtext.ide.util.CancelIndicatorProgressMonitor;
 import org.eclipse.xtext.resource.EObjectAtOffsetHelper;
+import org.eclipse.xtext.resource.IEObjectDescription;
+import org.eclipse.xtext.resource.IResourceDescription;
 import org.eclipse.xtext.resource.IResourceDescriptions;
 import org.eclipse.xtext.resource.IResourceServiceProvider;
 import org.eclipse.xtext.resource.XtextResource;
+import org.eclipse.xtext.service.OperationCanceledManager;
 import org.eclipse.xtext.util.CancelIndicator;
+import org.eclipse.xtext.xbase.lib.CollectionLiterals;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
@@ -49,6 +55,38 @@ public class XDocumentSymbolService extends DocumentSymbolService {
 
 	@Inject
 	private DocumentExtensions documentExtensions;
+
+	@Inject
+	private OperationCanceledManager operationCanceledManager;
+
+	// Overridden to wrap the call to #createSymbol inside resourceAccess.readOnly to re-use ResourceTaskContext
+	@Override
+	public List<? extends SymbolInformation> getSymbols(IResourceDescription resourceDescription, String query,
+			IResourceAccess resourceAccess, CancelIndicator cancelIndicator) {
+
+		List<IEObjectDescription> matchingObjects = CollectionLiterals.newLinkedList();
+		for (IEObjectDescription description : resourceDescription.getExportedObjects()) {
+			operationCanceledManager.checkCanceled(cancelIndicator);
+			if (filter(description, query)) {
+				matchingObjects.add(description);
+			}
+		}
+
+		if (matchingObjects.isEmpty()) {
+			return CollectionLiterals.newLinkedList();
+		}
+
+		AtomicReference<List<SymbolInformation>> refSymbols = new AtomicReference<>(CollectionLiterals.newLinkedList());
+		resourceAccess.readOnly(resourceDescription.getURI(), (resourceSet) -> {
+			List<SymbolInformation> symbols = refSymbols.get();
+			for (IEObjectDescription description : matchingObjects) {
+				operationCanceledManager.checkCanceled(cancelIndicator);
+				createSymbol(description, resourceAccess, symbol -> symbols.add(symbol));
+			}
+			return null;
+		});
+		return refSymbols.get();
+	}
 
 	// Overridden only to be able to factor out methods that accept custom reference acceptors:
 	@Override
