5a0ce4fcc468ac8c547240187aa6c9dc01666a28
mmews <marcus.mews@numberfour.eu>
2021-01-05 15:53:24 +0100

    revert changes

--- plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/server/build/N4JSBuildOrderInfoComputer.java
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/server/build/N4JSBuildOrderInfoComputer.java
@@ -10,10 +10,9 @@
  */
 package org.eclipse.n4js.ide.server.build;
 
-import java.util.HashSet;
+import java.util.Collections;
 import java.util.Set;
 
-import org.eclipse.n4js.N4JSGlobals;
 import org.eclipse.n4js.internal.lsp.N4JSProjectConfigSnapshot;
 import org.eclipse.n4js.projectDescription.ProjectType;
 import org.eclipse.n4js.xtext.workspace.BuildOrderFactory;
@@ -26,20 +25,13 @@ public class N4JSBuildOrderInfoComputer extends BuildOrderFactory.BuildOrderInfo
 
 	@Override
 	protected Set<String> getDependencies(ProjectConfigSnapshot pc) {
-		Set<String> dependencies = super.getDependencies(pc);
 		ProjectType type = pc instanceof N4JSProjectConfigSnapshot ? ((N4JSProjectConfigSnapshot) pc).getType() : null;
 		if (type == ProjectType.PLAINJS) {
-			// ignore plainJS dependencies of plain-JS projects, because
+			// ignore dependencies of plain-JS projects, because
 			// (1) they are irrelevant for the build order of N4JS code,
 			// (2) npm packages sometimes declare cyclic dependencies (and we must not show errors for those cycles)
-			Set<String> depsToScopeN4JSD = new HashSet<>();
-			for (String depName : dependencies) {
-				if (depName.startsWith(N4JSGlobals.N4JSD_SCOPE)) {
-					depsToScopeN4JSD.add(depName);
-				}
-			}
-			return depsToScopeN4JSD;
+			return Collections.emptySet();
 		}
-		return dependencies;
+		return super.getDependencies(pc);
 	}
 }
