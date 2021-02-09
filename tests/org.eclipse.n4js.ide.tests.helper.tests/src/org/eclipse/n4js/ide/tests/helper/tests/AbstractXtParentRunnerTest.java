e2d477d0cfca41e47af13d994409417d6370b1f3
mmews <marcus.mews@numberfour.eu>
2021-02-09 12:34:16 +0100

    first two tests for N4JS-Xpect

---
+++ tests/org.eclipse.n4js.ide.tests.helper.tests/src/org/eclipse/n4js/ide/tests/helper/tests/AbstractXtParentRunnerTest.java
@@ -0,0 +1,145 @@
+/**
+ * Copyright (c) 2021 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.ide.tests.helper.tests;
+
+import java.lang.annotation.Annotation;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import org.eclipse.n4js.ide.tests.helper.server.xt.XtFileRunner;
+import org.eclipse.n4js.ide.tests.helper.server.xt.XtFolder;
+import org.eclipse.n4js.ide.tests.helper.server.xt.XtParentRunner;
+import org.eclipse.n4js.utils.Strings;
+import org.eclipse.xtext.xbase.lib.Pair;
+import org.junit.Assert;
+import org.junit.runner.Description;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+import org.junit.runner.notification.RunListener;
+import org.junit.runner.notification.RunNotifier;
+
+/**
+ *
+ */
+public abstract class AbstractXtParentRunnerTest {
+
+	static class DynamicXtFolder implements XtFolder {
+		final String value;
+
+		/**  */
+		public DynamicXtFolder(String value) {
+			this.value = value;
+		}
+
+		@Override
+		public Class<? extends Annotation> annotationType() {
+			return XtFolder.class;
+		}
+
+		@Override
+		public String value() {
+			return value;
+		}
+	}
+
+	TestRunSimulator runListener;
+	Description parentDescription;
+	List<XtFileRunner> children;
+	List<Pair<String, Object>> events = new ArrayList<>();
+
+	class TestRunSimulator extends RunListener {
+
+		void run(String folderName) throws Exception {
+			XtFolder dynamicXtFolder = new DynamicXtFolder(folderName);
+
+			alterAnnotationOn(XtTestSetupTestMockup.class, XtFolder.class, dynamicXtFolder);
+
+			XtParentRunner xtParentRunner = new XtParentRunner(XtTestSetupTestMockup.class);
+			RunNotifier rn = new RunNotifier();
+			rn.addListener(this);
+
+			parentDescription = xtParentRunner.getDescription();
+			children = xtParentRunner.getChildren();
+			xtParentRunner.run(rn);
+		}
+
+		@Override
+		public void testRunStarted(Description description) throws Exception {
+			events.add(Pair.of("testRunStarted", description));
+		}
+
+		@Override
+		public void testRunFinished(Result result) throws Exception {
+			events.add(Pair.of("testRunFinished", result));
+		}
+
+		@Override
+		public void testStarted(Description description) throws Exception {
+			events.add(Pair.of("testStarted", description));
+		}
+
+		@Override
+		public void testFinished(Description description) throws Exception {
+			events.add(Pair.of("testFinished", description));
+		}
+
+		@Override
+		public void testFailure(Failure failure) throws Exception {
+			events.add(Pair.of("testFailure", failure));
+		}
+
+		@Override
+		public void testAssumptionFailure(Failure failure) {
+			events.add(Pair.of("testAssumptionFailure", failure));
+		}
+
+		@Override
+		public void testIgnored(Description description) throws Exception {
+			events.add(Pair.of("testIgnored", description));
+		}
+	}
+
+	void run(String folderName) throws Exception {
+		runListener = new TestRunSimulator();
+		runListener.run(folderName);
+	}
+
+	void assertEventNames(String eventNames) {
+		Assert.assertEquals(eventNames, Strings.toString(p -> p.getKey(), events));
+	}
+
+	static <A extends Annotation, A_NEW extends A> void alterAnnotationOn(Class<?> clazzToLookFor,
+			Class<A> annotationToAlter, A_NEW annotationValue) {
+
+		// Supported by JDK 8+
+		try {
+			// In JDK8 Class has a private method called annotationData().
+			// We first need to invoke it to obtain a reference to AnnotationData class which is a private class
+			Method method = Class.class.getDeclaredMethod("annotationData");
+			method.setAccessible(true);
+			// Since AnnotationData is a private class we cannot create a direct reference to it. We will have to
+			// manage with just Object
+			Object annotationData = method.invoke(clazzToLookFor);
+			// We now look for the map called "annotations" within AnnotationData object.
+			Field fieldAnnotations = annotationData.getClass().getDeclaredField("annotations");
+			fieldAnnotations.setAccessible(true);
+			@SuppressWarnings("unchecked")
+			Map<Class<? extends Annotation>, Annotation> map = (Map<Class<? extends Annotation>, Annotation>) fieldAnnotations
+					.get(annotationData);
+			map.put(annotationToAlter, annotationValue);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
---
+++ tests/org.eclipse.n4js.ide.tests.helper.tests/src/org/eclipse/n4js/ide/tests/helper/tests/XtTestSetupTestMockup.java
@@ -0,0 +1,21 @@
+/**
+ * Copyright (c) 2021 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.ide.tests.helper.tests;
+
+import org.eclipse.n4js.ide.tests.helper.server.xt.XtFolder;
+
+/**
+ *
+ */
+@XtFolder("dummy")
+public class XtTestSetupTestMockup {
+	// NOOP
+}
