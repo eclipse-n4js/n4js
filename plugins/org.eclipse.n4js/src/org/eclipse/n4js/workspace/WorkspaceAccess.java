eclipse-n4js/n4js

commit f69e1411d33da9fa0779ae205ac5902c28841731

WorkspaceAccess#getWorkspaceConfig() never returns null (or absent)

plugins/org.eclipse.n4js/src/org/eclipse/n4js/workspace/WorkspaceAccess.java
@@ -53,26 +53,25 @@ public class WorkspaceAccess {
 
 	/**
 	 * Returns the current workspace configuration of the context defined by the given EMF {@link ResourceSet} or
-	 * element contained in a resource set.
+	 * element contained in a resource set. Returns {@link N4JSWorkspaceConfigSnapshot#EMPTY} in case no workspace
+	 * configuration can be found. Never returns <code>null</code>.
 	 */
-	public Optional<N4JSWorkspaceConfigSnapshot> getWorkspaceConfig(Notifier context) {
+	public N4JSWorkspaceConfigSnapshot getWorkspaceConfig(Notifier context) {
 		ResourceSet resourceSet = EcoreUtil2.getResourceSet(context);
 		WorkspaceConfigSnapshot config = resourceSet != null
 				? WorkspaceConfigAdapter.getWorkspaceConfig(resourceSet)
 				: null;
-		return Optional.fromNullable((N4JSWorkspaceConfigSnapshot) config);
+		return config != null ? (N4JSWorkspaceConfigSnapshot) config : N4JSWorkspaceConfigSnapshot.EMPTY;
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#getProjects()}. */
 	public ImmutableSet<N4JSProjectConfigSnapshot> findAllProjects(Notifier context) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().getProjects() : ImmutableSet.of();
+		return getWorkspaceConfig(context).getProjects();
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#findProjectByPath(URI)}. */
 	public N4JSProjectConfigSnapshot findProjectByPath(Notifier context, URI path) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findProjectByPath(path) : null;
+		return getWorkspaceConfig(context).findProjectByPath(path);
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#findProjectByName(String)}. */
@@ -82,8 +81,7 @@ public class WorkspaceAccess {
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#findProjectByName(String)}. */
 	public N4JSProjectConfigSnapshot findProjectByName(Notifier context, String projectName) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findProjectByName(projectName) : null;
+		return getWorkspaceConfig(context).findProjectByName(projectName);
 	}
 
 	/**
@@ -93,8 +91,7 @@ public class WorkspaceAccess {
 	 * {@link ProjectSet#findProjectByNestedLocation(URI) HERE}.
 	 */
 	public N4JSProjectConfigSnapshot findProjectByNestedLocation(Notifier context, URI nestedLocation) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findProjectByNestedLocation(nestedLocation) : null;
+		return getWorkspaceConfig(context).findProjectByNestedLocation(nestedLocation);
 	}
 
 	/**
@@ -120,42 +117,39 @@ public class WorkspaceAccess {
 	 * {@link ProjectSet#findProjectByNestedLocation(URI) HERE}.
 	 */
 	public N4JSProjectConfigSnapshot findProjectContaining(Notifier context, URI nestedLocation) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findProjectContaining(nestedLocation) : null;
+		return getWorkspaceConfig(context).findProjectContaining(nestedLocation);
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#findSourceFolderContaining(URI)}. */
 	public N4JSSourceFolderSnapshot findSourceFolderContaining(Notifier context, URI nestedLocation) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findSourceFolderContaining(nestedLocation) : null;
+		return getWorkspaceConfig(context).findSourceFolderContaining(nestedLocation);
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#findProjectAndSourceFolderContaining(URI)}. */
 	public Pair<N4JSProjectConfigSnapshot, N4JSSourceFolderSnapshot> findProjectAndSourceFolderContaining(
 			Notifier context, URI nestedLocation) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() ? config.get().findProjectAndSourceFolderContaining(nestedLocation) : null;
+		return getWorkspaceConfig(context).findProjectAndSourceFolderContaining(nestedLocation);
 	}
 
 	/** Convenience for {@link N4JSWorkspaceConfigSnapshot#isNoValidate(URI)}. */
 	public boolean isNoValidate(Notifier context, URI nestedLocation) {
-		Optional<N4JSWorkspaceConfigSnapshot> config = getWorkspaceConfig(context);
-		return config.isPresent() && config.get().isNoValidate(nestedLocation);
+		return getWorkspaceConfig(context).isNoValidate(nestedLocation);
 	}
 
 	// ######################################################################################################
 	// The following methods are included here for historic reasons:
 
 	/**
-	 * Creates a new resource set and configures it with the "current" state of the "default workspace", if possible. It
-	 * is undefined which workspace will be used (in case several exist) and which exact point in time its state will
-	 * represent. <b>Therefore, this method should only be used when setting up an independent environment from scratch
-	 * (e.g. some external tooling).</b>
+	 * Creates a new resource set and configures it with the "current" state of the "default workspace". It is undefined
+	 * which workspace will be used (in case several exist) and which exact point in time its state will represent.
+	 * <b>Therefore, this method should only be used when setting up an independent environment from scratch (e.g. some
+	 * external tooling).</b>
 	 * <p>
-	 * Because no workspace context is defined on the level of bundle <code>org.eclispe.n4js</code> this method will not
-	 * configure any workspace, by default. In the IDE context, the current workspace configuration of the LSP builder's
-	 * {@code ConcurrentIndex} will be used (configure in override of this method). In tests, a mock workspace
-	 * configuration will be used (configured via {@code MockResourceSetProvider}).
+	 * Because no workspace context is defined on the level of bundle <code>org.eclispe.n4js</code> this method will use
+	 * an {@link N4JSWorkspaceConfigSnapshot#EMPTY EMPTY} workspace configuration, by default. In the IDE context, the
+	 * current workspace configuration of the LSP builder's {@code ConcurrentIndex} will be used (configured in override
+	 * of this method in subclass {@code IdeWorkspaceAccess}). In tests, a mock workspace configuration will be used
+	 * (configured indirectly via {@code MockResourceSetProvider}).
 	 * <p>
 	 * IMPORTANT:
 	 * <ul>
@@ -169,7 +163,9 @@ public class WorkspaceAccess {
 	 * </ul>
 	 */
 	public XtextResourceSet createResourceSet() {
-		return resourceSetProvider.get();
+		XtextResourceSet result = resourceSetProvider.get();
+		WorkspaceConfigAdapter.installWorkspaceConfig(result, N4JSWorkspaceConfigSnapshot.EMPTY);
+		return result;
 	}
 
 	/**

plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/editor/contentassist/ImportsAwareReferenceProposalCreator.java
@@ -23,8 +23,8 @@ import org.eclipse.n4js.ide.imports.ReferenceResolutionFinder.IResolutionAccepto
 import org.eclipse.n4js.n4JS.N4JSPackage;
 import org.eclipse.n4js.resource.N4JSResource;
 import org.eclipse.n4js.ts.types.TypesPackage;
-import org.eclipse.n4js.workspace.WorkspaceAccess;
 import org.eclipse.n4js.workspace.N4JSWorkspaceConfigSnapshot;
+import org.eclipse.n4js.workspace.WorkspaceAccess;
 import org.eclipse.xtext.ide.editor.contentassist.ContentAssistContext;
 import org.eclipse.xtext.ide.editor.contentassist.ContentAssistEntry;
 import org.eclipse.xtext.ide.editor.contentassist.IIdeContentProposalAcceptor;
@@ -80,7 +80,7 @@ public class ImportsAwareReferenceProposalCreator {
 		}
 		N4JSResource resourceCasted = (N4JSResource) resource;
 
-		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(resourceCasted).get();
+		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(resourceCasted);
 		ReferenceDescriptor referenceDesc = new ReferenceDescriptor(context.getPrefix(), model, reference,
 				context.getCurrentNode());
 		Predicate<String> conflictChecker = (proposalToCheck) -> conflictHelper.existsConflict(proposalToCheck,

plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/imports/ImportHelper.java
@@ -35,8 +35,8 @@ import org.eclipse.n4js.tooling.organizeImports.ImportSpecifiersUtil;
 import org.eclipse.n4js.ts.typeRefs.ParameterizedTypeRef;
 import org.eclipse.n4js.ts.typeRefs.TypeRefsPackage;
 import org.eclipse.n4js.ts.types.TModule;
-import org.eclipse.n4js.workspace.WorkspaceAccess;
 import org.eclipse.n4js.workspace.N4JSWorkspaceConfigSnapshot;
+import org.eclipse.n4js.workspace.WorkspaceAccess;
 import org.eclipse.n4js.workspace.utils.N4JSProjectName;
 import org.eclipse.xtext.conversion.IValueConverterService;
 import org.eclipse.xtext.linking.lazy.LazyLinkingResource;
@@ -179,7 +179,7 @@ public class ImportHelper {
 
 		triggerProxyResolution(script, cancelIndicator);
 
-		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(script).get();
+		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(script);
 		List<ReferenceResolution> result = new ArrayList<>();
 		Set<String> donePrefixes = new HashSet<>();
 		Iterator<EObject> iter = script.eAllContents();
@@ -215,7 +215,7 @@ public class ImportHelper {
 
 		triggerProxyResolution(astNode, cancelIndicator);
 
-		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(astNode).get();
+		N4JSWorkspaceConfigSnapshot wc = workspaceAccess.getWorkspaceConfig(astNode);
 
 		ReferenceDescriptor reference = getUnresolvedReferenceForASTNode(astNode);
 		if (reference == null) {

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/ConfigSnapshotFactory.java
@@ -33,7 +33,7 @@ public class ConfigSnapshotFactory {
 	@Inject
 	protected BuildOrderFactory buildOrderFactory;
 
-	/** Creates an instance of {@link WorkspaceConfigSnapshot} representing an empty workspace. */
+	/** Creates an empty instance of {@link WorkspaceConfigSnapshot} representing an empty workspace. */
 	public WorkspaceConfigSnapshot createWorkspaceConfigSnapshot(URI path) {
 		return createWorkspaceConfigSnapshot(path, Collections.emptyList());
 	}

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/ProjectSet.java
@@ -34,6 +34,9 @@ import com.google.common.collect.SetMultimap;
  */
 public class ProjectSet {
 
+	/** An empty project set. */
+	public static final ProjectSet EMPTY = new ProjectSet(Collections.emptyList());
+
 	/** Map from {@link ProjectConfigSnapshot#getName() project name} to the project's configuration snapshot. */
 	protected final ImmutableBiMap<String, ? extends ProjectConfigSnapshot> name2Project;
 	/** Inverse dependencies, i.e. map from name of a project P to all projects with a dependency to P. */
@@ -140,9 +143,14 @@ public class ProjectSet {
 		}
 	}
 
+	/** Tells whether this project set is empty. */
+	public boolean isEmpty() {
+		return name2Project.isEmpty();
+	}
+
 	/** Returns the number of projects in this project set. */
 	public int size() {
-		return name2Project.size();
+		return name2Project.values().size();
 	}
 
 	/** Tells whether the given project is contained in this set. */

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/WorkspaceConfigSnapshot.java
@@ -25,6 +25,13 @@ import com.google.common.collect.ImmutableSet;
 @SuppressWarnings("restriction")
 public class WorkspaceConfigSnapshot extends Snapshot {
 
+	/** The path used for the {@link #EMPTY default empty} workspace configuration. */
+	public static final URI EMPTY_PATH = URI.createFileURI("/empty-workspace");
+
+	/** An empty workspace configuration with a {@link #EMPTY_PATH default path}. */
+	public static final WorkspaceConfigSnapshot EMPTY = new WorkspaceConfigSnapshot(EMPTY_PATH, ProjectSet.EMPTY,
+			BuildOrderInfo.NULL);
+
 	/** The root path of the workspace */
 	protected final URI path;
 	/** Set of projects in the workspace, prepared for fast lookup by name, path, etc. */
@@ -42,6 +49,16 @@ public class WorkspaceConfigSnapshot extends Snapshot {
 	/** Getter for the root path. */
 	public URI getPath() {
 		return path;
+	}
+
+	/** Tells whether this workspace is empty, i.e. does not contain any projects. */
+	public boolean isEmpty() {
+		return projects.isEmpty();
+	}
+
+	/** Number of projects in this workspace. */
+	public int size() {
+		return projects.size();
 	}
 
 	/** Get all the projects known in this snapshot. */

plugins/org.eclipse.n4js/src/org/eclipse/n4js/generator/AbstractSubGenerator.xtend
@@ -28,9 +28,9 @@ import org.eclipse.n4js.utils.ResourceNameComputer
 import org.eclipse.n4js.utils.StaticPolyfillHelper
 import org.eclipse.n4js.utils.URIUtils
 import org.eclipse.n4js.validation.helper.FolderContainmentHelper
-import org.eclipse.n4js.workspace.WorkspaceAccess
 import org.eclipse.n4js.workspace.N4JSProjectConfigSnapshot
 import org.eclipse.n4js.workspace.N4JSWorkspaceConfigSnapshot
+import org.eclipse.n4js.workspace.WorkspaceAccess
 import org.eclipse.xtend.lib.annotations.Accessors
 import org.eclipse.xtext.generator.AbstractFileSystemAccess
 import org.eclipse.xtext.generator.IFileSystemAccess
@@ -116,10 +116,7 @@ abstract class AbstractSubGenerator implements ISubGenerator, IGenerator2 {
 	 * </ul>
 	 */
 	override doGenerate(Resource input, IFileSystemAccess fsa) {
-		val ws = workspaceAccess.getWorkspaceConfig(input).orNull;
-		if (ws === null) {
-			return;
-		}
+		val ws = workspaceAccess.getWorkspaceConfig(input);
 
 		try {
 			// remove error-marker
@@ -151,10 +148,7 @@ abstract class AbstractSubGenerator implements ISubGenerator, IGenerator2 {
 	}
 
 	override shouldBeCompiled(Resource input, CancelIndicator monitor) {
-		val ws = workspaceAccess.getWorkspaceConfig(input).orNull;
-		if (ws === null) {
-			return false;
-		}
+		val ws = workspaceAccess.getWorkspaceConfig(input);
 
 		val autobuildEnabled = isActive(input)
 		val isXPECTMode = N4JSGlobals.XT_FILE_EXTENSION == input.URI.fileExtension.toLowerCase

plugins/org.eclipse.n4js/src/org/eclipse/n4js/scoping/N4JSScopeProvider.xtend
@@ -362,10 +362,7 @@ class N4JSScopeProvider extends AbstractScopeProvider implements IDelegatingScop
 		val delegateMainModuleAwareScope = MainModuleAwareSelectableBasedScope.createMainModuleAwareScope(initialScope,
 			resourceDescriptions, reference.EReferenceType);
 
-if (!workspaceAccess.getWorkspaceConfig(resource).isPresent()) {
-	println("!!!")
-}
-		val ws = workspaceAccess.getWorkspaceConfig(resource).get();
+		val ws = workspaceAccess.getWorkspaceConfig(resource);
 		val projectImportEnabledScope = ProjectImportEnablingScope.create(ws, resource, importDeclaration,
 			initialScope, delegateMainModuleAwareScope);
 

commit d7183c5c156d289e23280caaa443c0b30d2cac62

fix a problem introduced by f69e141

plugins/org.eclipse.n4js/src/org/eclipse/n4js/workspace/WorkspaceAccess.java
@@ -164,7 +164,10 @@ public class WorkspaceAccess {
 	 */
 	public XtextResourceSet createResourceSet() {
 		XtextResourceSet result = resourceSetProvider.get();
-		WorkspaceConfigAdapter.installWorkspaceConfig(result, N4JSWorkspaceConfigSnapshot.EMPTY);
+		// use EMPTY iff the resource set provider did not install a workspace config (e.g. MockResourceSetProvider):
+		if (WorkspaceConfigAdapter.getWorkspaceConfig(result) == null) {
+			WorkspaceConfigAdapter.installWorkspaceConfig(result, N4JSWorkspaceConfigSnapshot.EMPTY);
+		}
 		return result;
 	}
 