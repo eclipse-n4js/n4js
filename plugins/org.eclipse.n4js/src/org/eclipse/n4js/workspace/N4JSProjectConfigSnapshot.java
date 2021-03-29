eclipse-n4js/n4js

commit c7fa8fc3261c5ca0bcfdf5e02ae3fc29bc814190

clean up snapshot classes and related code

plugins/org.eclipse.n4js/src/org/eclipse/n4js/workspace/N4JSProjectConfigSnapshot.java
@@ -24,8 +24,8 @@ import org.eclipse.n4js.packagejson.projectDescription.ProjectReference;
 import org.eclipse.n4js.packagejson.projectDescription.ProjectType;
 import org.eclipse.n4js.semver.Semver.VersionNumber;
 import org.eclipse.n4js.utils.ModuleFilterUtils;
+import org.eclipse.n4js.utils.URIUtils;
 import org.eclipse.n4js.workspace.locations.FileURI;
-import org.eclipse.n4js.workspace.locations.SafeURI;
 import org.eclipse.n4js.workspace.utils.N4JSProjectName;
 import org.eclipse.n4js.xtext.workspace.ProjectConfigSnapshot;
 import org.eclipse.n4js.xtext.workspace.SourceFolderSnapshot;
@@ -41,6 +41,7 @@ import com.google.common.collect.ImmutableSet;
 public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 
 	private final ProjectDescription projectDescription;
+	private final boolean external;
 
 	/** Creates a new {@link N4JSProjectConfigSnapshot}. */
 	public N4JSProjectConfigSnapshot(ProjectDescription projectDescription, URI path,
@@ -48,17 +49,32 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 			Iterable<? extends SourceFolderSnapshot> sourceFolders) {
 
 		super(projectDescription.getProjectName(), path,
-				Collections.singleton(path.trimSegments(1).appendSegment(N4JSGlobals.PACKAGE_JSON)),
+				Collections.singleton(URIUtils.trimTrailingPathSeparator(path).trimSegments(1)
+						.appendSegment(N4JSGlobals.PACKAGE_JSON)),
 				indexOnly, generatorEnabled, dependencies, sourceFolders);
 
 		this.projectDescription = Objects.requireNonNull(projectDescription);
+		this.external = isDirectlyLocatedInNodeModulesFolder(path);
+	}
+
+	private static boolean isDirectlyLocatedInNodeModulesFolder(URI location) {
+		URI parent = URIUtils.trimTrailingPathSeparator(location).trimSegments(1);
+		String lastSegment = parent.lastSegment();
+		if (lastSegment != null && lastSegment.startsWith("@")) {
+			parent = parent.trimSegments(1);
+			lastSegment = parent.lastSegment();
+		}
+		if (N4JSGlobals.NODE_MODULES.equals(lastSegment)) {
+			return true;
+		}
+		return false;
 	}
 
 	/**
 	 * Returns the project dependencies.
 	 * <p>
 	 * Note that this method does not return the {@link N4JSProjectConfig#getDependencies() raw dependencies} as given
-	 * in the <code>package.json</code> but the {@link N4JSProjectConfig#computeSemanticDependencies() semantic
+	 * in the <code>package.json</code> but the {@link N4JSProjectConfig#computeSemanticDependencies() "semantic"
 	 * dependencies} computed by class {@link N4JSProjectConfig}.
 	 */
 	@Override
@@ -69,6 +85,11 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 	/** Returns the {@link ProjectDescription}. */
 	public ProjectDescription getProjectDescription() {
 		return projectDescription;
+	}
+
+	/** Tells whether this project is located in a <code>node_modules</code> folder. */
+	public boolean isExternal() {
+		return external;
 	}
 
 	/** Returns the value of the {@link PackageJsonProperties#DEFINES_PACKAGE "definesPackage"} property. */
@@ -94,8 +115,14 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 	// ==============================================================================================================
 	// Convenience and utility methods (do not introduce additional data)
 
+	/** This project's name as an {@link N4JSProjectName}. */
 	public N4JSProjectName getN4JSProjectName() {
 		return new N4JSProjectName(getName());
+	}
+
+	/** Returns this project's {@link #getPath() path} as a {@link FileURI}. */
+	public FileURI getPathAsFileURI() {
+		return new FileURI(new UriExtensions().withEmptyAuthority(getPath()));
 	}
 
 	@Override
@@ -110,45 +137,62 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 		return (N4JSSourceFolderSnapshot) super.findSourceFolderContaining(uri);
 	}
 
-	/** Returns the {@link ProjectType project type}. */
+	/** Returns this project's {@link ProjectDescription#getProjectType() type}. */
 	public ProjectType getType() {
 		return projectDescription.getProjectType();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getProjectVersion() version}. */
 	public VersionNumber getVersion() {
 		return projectDescription.getProjectVersion();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getVendorId() vendor ID}. */
 	public String getVendorId() {
 		return projectDescription.getVendorId();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getVendorName() vendor name}. */
 	public String getVendorName() {
 		return projectDescription.getVendorName();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getOutputPath() output path}. */
 	public String getOutputPath() {
 		return projectDescription.getOutputPath();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getMainModule() main module}. */
 	public String getMainModule() {
 		return projectDescription.getMainModule();
 	}
 
+	/** Returns this project's {@link ProjectDescription#getImplementationId() implementation ID}. */
 	public String getImplementationId() {
 		return projectDescription.getImplementationId();
 	}
 
-	// FIXME reconsider!
-	public FileURI getPathAsFileURI() {
-		return new FileURI(new UriExtensions().withEmptyAuthority(getPath()));
+	/**
+	 * Returns this project's {@link ProjectDescription#getProjectDependencies() dependencies} and
+	 * {@link ProjectDescription#getImplementedProjects() implemented projects} (in this order).
+	 */
+	public ImmutableList<ProjectReference> getDependenciesAndImplementedApis() {
+		ImmutableList.Builder<ProjectReference> result = ImmutableList.builder();
+		result.addAll(projectDescription.getProjectDependencies());
+		result.addAll(projectDescription.getImplementedProjects());
+		return result.build();
 	}
 
+	/**
+	 * Tells whether the given nested location URI is matched by a module filter in this project's
+	 * <code>package.json</code> file, only taking into account module filters of the given type.
+	 */
 	public boolean isMatchedByModuleFilterOfType(URI nestedLocation, ModuleFilterType moduleFilterType) {
 		return ModuleFilterUtils.isPathContainedByFilter(this, nestedLocation,
 				getModuleFilterSpecifiersByType(moduleFilterType));
 	}
 
+	/** Returns this project's {@link ProjectDescription#getModuleFilters() module filers} of the given type */
 	public ImmutableList<ModuleFilterSpecifier> getModuleFilterSpecifiersByType(ModuleFilterType moduleFilterType) {
 		Builder<ModuleFilterSpecifier> result = ImmutableList.builder();
 		for (ModuleFilter mf : projectDescription.getModuleFilters()) {
@@ -156,38 +200,6 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 				result.addAll(mf.getModuleSpecifiers());
 			}
 		}
-		return result.build();
-	}
-
-	// FIXME consider caching this value
-	public boolean isExternal() {
-		return isInNodeModulesFolder(getPathAsFileURI());
-	}
-
-	// note: implementation taken from old class org.eclipse.n4js.internal.N4JSProject
-	private static boolean isInNodeModulesFolder(SafeURI<?> location) {
-		if (location instanceof FileURI) {
-			URI parent = location.getParent().toURI();
-			String lastSegment = parent.lastSegment();
-			if (parent.lastSegment() != null && parent.lastSegment().isBlank()) {
-				parent = parent.trimSegments(1);
-				lastSegment = parent.lastSegment();
-			}
-			if (lastSegment != null && lastSegment.startsWith("@")) {
-				parent = parent.trimSegments(1);
-				lastSegment = parent.lastSegment();
-			}
-			if (N4JSGlobals.NODE_MODULES.equals(lastSegment)) {
-				return true;
-			}
-		}
-		return false;
-	}
-
-	public ImmutableList<ProjectReference> getDependenciesAndImplementedApis() {
-		ImmutableList.Builder<ProjectReference> result = ImmutableList.builder();
-		result.addAll(projectDescription.getProjectDependencies());
-		result.addAll(projectDescription.getImplementedProjects());
 		return result.build();
 	}
 }

plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/IdeWorkspaceAccess.java
@@ -11,18 +11,26 @@
 package org.eclipse.n4js.ide;
 
 import org.eclipse.n4js.workspace.WorkspaceAccess;
+import org.eclipse.n4js.xtext.ide.server.ResourceTaskContext;
 import org.eclipse.n4js.xtext.ide.server.ResourceTaskManager;
 import org.eclipse.xtext.resource.XtextResourceSet;
 
 import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
+/**
+ * Adjustments of {@link WorkspaceAccess} when running in the LSP context.
+ */
 @Singleton
 public class IdeWorkspaceAccess extends WorkspaceAccess {
 
 	@Inject
 	private ResourceTaskManager resourceTaskManager;
 
+	/**
+	 * This delegates to {@link ResourceTaskManager} for creating resource sets, to make sure they are set up the same
+	 * way as those used in {@link ResourceTaskContext}s.
+	 */
 	@Override
 	public XtextResourceSet createResourceSet() {
 		return resourceTaskManager.createTemporaryResourceSet();

plugins/org.eclipse.n4js.xtext.ide/src/org/eclipse/n4js/xtext/ide/server/ResourceTaskContext.java
@@ -429,21 +429,7 @@ public class ResourceTaskContext {
 			IResourceDescription.Manager rdm = getResourceDescriptionManager(mainURI);
 			IResourceDescription candidateDesc = indexSnapshot.getResourceDescription(mainURI);
 			if (rdm != null && candidateDesc != null) {
-				if (rdm instanceof AllChangeAware) {
-					isAffected = ((AllChangeAware) rdm).isAffectedByAny(allDeltas, candidateDesc, indexSnapshot);
-				} else {
-					List<IResourceDescription.Delta> changedDeltas = allDeltas.stream()
-							.filter(d -> d.haveEObjectDescriptionsChanged())
-							.collect(Collectors.toList());
-					if (!changedDeltas.isEmpty()) {
-						if (rdm instanceof IWorkspaceAwareResourceDescriptionManager) {
-							isAffected = ((IWorkspaceAwareResourceDescriptionManager) rdm).isAffected(changedDeltas,
-									candidateDesc, indexSnapshot, workspaceConfig);
-						} else {
-							isAffected = rdm.isAffected(changedDeltas, candidateDesc, indexSnapshot);
-						}
-					}
-				}
+				isAffected = isAffected(candidateDesc, rdm, allDeltas);
 			}
 		}
 
@@ -452,6 +438,12 @@ public class ResourceTaskContext {
 		}
 	}
 
+	/**
+	 * Checks whether the given candidate resource description is affected by the given deltas, by delegating to the
+	 * given resource description manager.
+	 * <p>
+	 * Compare with method {@code XIndexer#isAffected(...)}.
+	 */
 	protected boolean isAffected(IResourceDescription candidateDesc, IResourceDescription.Manager manager,
 			Collection<IResourceDescription.Delta> allDeltas) {
 

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/ConfigSnapshotFactory.java
@@ -107,10 +107,12 @@ public class ConfigSnapshotFactory {
 		return new SourceFolderSnapshot(sourceFolder.getName(), sourceFolder.getPath());
 	}
 
+	/** Creates instances of {@link ProjectSet}. */
 	public ProjectSet createProjectSet(Iterable<? extends ProjectConfigSnapshot> projects) {
 		return new ProjectSet(projects);
 	}
 
+	/** Updates the given project set by adding, changing, removing some projects. */
 	public ProjectSet updateProjectSet(ProjectSet projects,
 			Iterable<? extends ProjectConfigSnapshot> changedProjects, Iterable<String> removedProjects) {
 		return projects.update(changedProjects, removedProjects);

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/ProjectSet.java
@@ -186,19 +186,24 @@ public class ProjectSet {
 	 * Finds the project having a path that is a prefix of the given URI. Returns <code>null</code> if no such project
 	 * is found. The given URI may point to a file or folder.
 	 * <p>
-	 * Note the difference to {@link #findProjectContaining(URI)}: this method will return a project <code>P</code> when
-	 * given any URI denoting a file/folder below the path of <code>P</code>, whereas {@code #findProjectContaining()}
-	 * will return <code>P</code> only when given URIs {@link SourceFolderSnapshot#contains(URI) actually contained} in
-	 * one of <code>P</code>'s source folders.
+	 * Note the difference to {@link #findProjectContaining(URI)} which will return <code>P</code> only when given a URI
+	 * that is {@link SourceFolderSnapshot#contains(URI) actually contained} in one of <code>P</code>'s source folders,
+	 * whereas this method will return a project <code>P</code> when given any URI denoting a file/folder below the path
+	 * of <code>P</code>.
 	 */
 	public ProjectConfigSnapshot findProjectByNestedLocation(URI nestedLocation) {
 		return URIUtils.findInMapByNestedURI(projectPath2Project, nestedLocation);
 	}
 
 	/**
-	 * Same as {@link XIWorkspaceConfig#findProjectContaining(URI)}: finds the project having a source folder that
-	 * {@link SourceFolderSnapshot#contains(URI) actually contains} the given nested URI. Returns <code>null</code> if
-	 * no such project is found. The given URI may point to a file or folder.
+	 * Finds the project having a source folder that {@link SourceFolderSnapshot#contains(URI) actually contains} the
+	 * given nested URI. Returns <code>null</code> if no such project is found. The given URI may point to a file or
+	 * folder.
+	 * <p>
+	 * Note the difference to {@link #findProjectByNestedLocation(URI)} which will return a project <code>P</code> when
+	 * given any URI denoting a file/folder below the path of <code>P</code>, whereas this method will return
+	 * <code>P</code> only when given a URI that is {@link SourceFolderSnapshot#contains(URI) actually contained} in one
+	 * of <code>P</code>'s source folders.
 	 *
 	 * @see SourceFolderSnapshot#contains(URI)
 	 * @see XIWorkspaceConfig#findProjectContaining(URI)
@@ -209,18 +214,14 @@ public class ProjectSet {
 		return result != null ? result.getKey() : null;
 	}
 
-	/**
-	 * Same as {@link #findProjectContaining(URI)}, but returns the containing source folder instead of project.
-	 */
+	/** Same as {@link #findProjectContaining(URI)}, but returns the containing source folder instead of project. */
 	public SourceFolderSnapshot findSourceFolderContaining(URI nestedSourceLocation) {
 		Pair<ProjectConfigSnapshot, SourceFolderSnapshot> result = findProjectAndSourceFolderContaining(
 				nestedSourceLocation);
 		return result != null ? result.getValue() : null;
 	}
 
-	/**
-	 * Same as {@link #findProjectContaining(URI)}, but returns both the containing project and source folder.
-	 */
+	/** Same as {@link #findProjectContaining(URI)}, but returns both the containing project and source folder. */
 	public Pair<ProjectConfigSnapshot, SourceFolderSnapshot> findProjectAndSourceFolderContaining(
 			URI nestedSourceLocation) {
 		ProjectConfigSnapshot candidate = URIUtils.findInMapByNestedURI(sourceFolderPath2Project, nestedSourceLocation);

plugins/org.eclipse.n4js.xtext/src/org/eclipse/n4js/xtext/workspace/WorkspaceConfigAdapter.java
@@ -10,6 +10,8 @@
  */
 package org.eclipse.n4js.xtext.workspace;
 
+import java.util.Objects;
+
 import org.eclipse.emf.common.notify.impl.AdapterImpl;
 import org.eclipse.emf.ecore.resource.ResourceSet;
 import org.eclipse.emf.ecore.util.EcoreUtil;
@@ -21,6 +23,7 @@ import org.eclipse.n4js.utils.EcoreUtilN4;
  */
 public class WorkspaceConfigAdapter extends AdapterImpl {
 
+	/** Obtain the workspace configuration attached to the given resource set. */
 	public static WorkspaceConfigSnapshot getWorkspaceConfig(ResourceSet resourceSet) {
 		if (resourceSet == null) {
 			return null;
@@ -43,6 +46,7 @@ public class WorkspaceConfigAdapter extends AdapterImpl {
 		return null;
 	}
 
+	/** Attach the given workspace configuration to the given resource set. */
 	public static void installWorkspaceConfig(ResourceSet resourceSet, WorkspaceConfigSnapshot workspaceConfig) {
 		if (resourceSet == null) {
 			return;
@@ -53,6 +57,7 @@ public class WorkspaceConfigAdapter extends AdapterImpl {
 		}, resourceSet);
 	}
 
+	/** Remove the workspace configuration attached to the given resource set (if any). */
 	public static void uninstallWorkspaceConfig(ResourceSet resourceSet) {
 		if (resourceSet == null) {
 			return;
@@ -71,5 +76,10 @@ public class WorkspaceConfigAdapter extends AdapterImpl {
 	@Override
 	public boolean isAdapterForType(Object type) {
 		return type == WorkspaceConfigAdapter.class;
+	}
+
+	@Override
+	public String toString() {
+		return getClass().getSimpleName() + " --> " + Objects.toString(workspaceConfig);
 	}
 }

plugins/org.eclipse.n4js/src/org/eclipse/n4js/generator/AbstractSubGenerator.xtend
@@ -172,7 +172,7 @@ abstract class AbstractSubGenerator implements ISubGenerator, IGenerator2 {
 	}
 
 	private def hasOutput(N4JSWorkspaceConfigSnapshot ws, URI n4jsSourceURI){
-		return ws.getOutputPath(n4jsSourceURI) !== null;
+		return getOutputPath(ws, n4jsSourceURI) !== null;
 	}
 	private def isSource(N4JSWorkspaceConfigSnapshot ws, URI n4jsSourceURI) {
 		return ws.findSourceFolderContaining(n4jsSourceURI) !== null;
@@ -300,13 +300,17 @@ abstract class AbstractSubGenerator implements ISubGenerator, IGenerator2 {
 
 	/** Adjust output-path of the generator to match the N4JS projects-settings. */
 	private def void updateOutputPath(N4JSWorkspaceConfigSnapshot ws, IFileSystemAccess fsa, String compilerID, Resource input) {
-		val outputPath = ws.getOutputPath(input.URI) ?: N4JSLanguageConstants.DEFAULT_PROJECT_OUTPUT;
+		val outputPath = getOutputPath(ws, input.URI) ?: N4JSLanguageConstants.DEFAULT_PROJECT_OUTPUT;
 		if (fsa instanceof AbstractFileSystemAccess) {
 			val conf = fsa.outputConfigurations.get(compilerID)
 			if (conf !== null) {
 				conf.setOutputDirectory(outputPath)
 			}
 		}
+	}
+
+	private static def String getOutputPath(N4JSWorkspaceConfigSnapshot ws, URI nestedLocation) {
+		return ws.findProjectContaining(nestedLocation)?.getProjectDescription()?.getOutputPath();
 	}
 
 	/** Navigation from the generated output-location to the location of the input-resource  */
@@ -425,5 +429,4 @@ abstract class AbstractSubGenerator implements ISubGenerator, IGenerator2 {
 	override boolean isApplicableTo(Resource input) {
 		return shouldBeCompiled(input, null);
 	}
-
 }

plugins/org.eclipse.n4js/src/org/eclipse/n4js/utils/FindArtifactHelper.java
@@ -10,9 +10,12 @@
  */
 package org.eclipse.n4js.utils;
 
+import java.util.List;
+
 import org.eclipse.emf.common.util.URI;
 import org.eclipse.n4js.workspace.N4JSProjectConfigSnapshot;
 import org.eclipse.n4js.workspace.N4JSSourceFolderSnapshot;
+import org.eclipse.n4js.workspace.locations.FileURI;
 import org.eclipse.n4js.workspace.locations.SafeURI;
 import org.eclipse.xtext.naming.IQualifiedNameConverter;
 import org.eclipse.xtext.naming.QualifiedName;
@@ -22,8 +25,7 @@ import com.google.inject.Inject;
 import com.google.inject.Singleton;
 
 /**
- * Helper for finding artifacts in the projects. Delegates to
- * {@link N4JSSourceFolderSnapshot#findArtifact(QualifiedName, Optional)} but exposes a more convenient API.
+ * Helper for finding artifacts in the projects.
  */
 @Singleton
 public final class FindArtifactHelper {
@@ -40,16 +42,46 @@ public final class FindArtifactHelper {
 	}
 
 	/**
-	 * Convenience method for {@link N4JSSourceFolderSnapshot#findArtifact(QualifiedName, Optional)}, searching all
+	 * Convenience method for {@link #findArtifact(N4JSSourceFolderSnapshot, QualifiedName, Optional)}, searching all
 	 * source folders of the given project.
 	 */
 	public URI findArtifact(N4JSProjectConfigSnapshot project, QualifiedName fqn, Optional<String> fileExtension) {
 		for (N4JSSourceFolderSnapshot srcFolder : project.getSourceFolders()) {
-			final SafeURI<?> uri = srcFolder.findArtifact(fqn, fileExtension);
+			final SafeURI<?> uri = findArtifact(srcFolder, fqn, fileExtension);
 			if (uri != null)
 				return uri.toURI();
 		}
 		return null;
 	}
 
+	/**
+	 * If the receiving source folder actually contains a file for the given fully qualified name and file extension on
+	 * disk, this method will return a URI for this file of the same format as the URIs returned by method
+	 * {@link N4JSSourceFolderSnapshot#getContents()}. Otherwise, this method returns <code>null</code>.
+	 * <p>
+	 * The file extension may but need not contain a leading '.'.
+	 * <p>
+	 * Implementations are expected to be optimized for fast look-up (in particular, they should avoid iterating over
+	 * all URIs returned by method {@link N4JSSourceFolderSnapshot#getContents()}).
+	 */
+	public FileURI findArtifact(N4JSSourceFolderSnapshot sourceFolder, QualifiedName name,
+			Optional<String> fileExtension) {
+
+		final List<String> nameSegments = name.getSegments();
+		if (nameSegments.isEmpty()) {
+			return null;
+		}
+		final String[] nameSegmentsCpy = nameSegments.toArray(new String[nameSegments.size()]);
+		final String ext = fileExtension.or("").trim();
+		final String extWithDot = !ext.isEmpty() && !ext.startsWith(".") ? "." + ext : ext;
+		final int idxLast = nameSegmentsCpy.length - 1;
+		nameSegmentsCpy[idxLast] = nameSegmentsCpy[idxLast] + extWithDot;
+
+		FileURI sourceFolderPath = sourceFolder.getPathAsFileURI();
+		FileURI result = sourceFolderPath.appendSegments(nameSegmentsCpy);
+		if (result.exists()) {
+			return result;
+		}
+		return null;
+	}
 }

commit 16ee0d9db50e986306a8f0c3e52aee1ef80ff100

fix a problem introduced by c7fa8fc

plugins/org.eclipse.n4js/src/org/eclipse/n4js/workspace/N4JSProjectConfigSnapshot.java
@@ -49,8 +49,7 @@ public class N4JSProjectConfigSnapshot extends ProjectConfigSnapshot {
 			Iterable<? extends SourceFolderSnapshot> sourceFolders) {
 
 		super(projectDescription.getName(), path,
-				Collections.singleton(URIUtils.trimTrailingPathSeparator(path).trimSegments(1)
-						.appendSegment(N4JSGlobals.PACKAGE_JSON)),
+				Collections.singleton(URIUtils.trimTrailingPathSeparator(path).appendSegment(N4JSGlobals.PACKAGE_JSON)),
 				indexOnly, generatorEnabled, dependencies, sourceFolders);
 
 		this.projectDescription = Objects.requireNonNull(projectDescription);