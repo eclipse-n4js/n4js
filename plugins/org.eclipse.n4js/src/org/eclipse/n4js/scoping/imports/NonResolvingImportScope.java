a4c6c7b5ecabe1fd5a299debd6e306d6835aac50
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2021-04-01 13:21:27 +0200

    fix 3rd problem (tentative)

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/scoping/imports/NonResolvingImportScope.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/scoping/imports/NonResolvingImportScope.java
@@ -16,6 +16,7 @@ import static java.util.Collections.emptyList;
 import java.util.List;
 
 import org.eclipse.emf.ecore.EClass;
+import org.eclipse.n4js.utils.collections.Iterables2;
 import org.eclipse.xtext.naming.QualifiedName;
 import org.eclipse.xtext.resource.IEObjectDescription;
 import org.eclipse.xtext.resource.ISelectable;
@@ -23,6 +24,12 @@ import org.eclipse.xtext.resource.impl.AliasedEObjectDescription;
 import org.eclipse.xtext.scoping.IScope;
 import org.eclipse.xtext.scoping.impl.ImportNormalizer;
 import org.eclipse.xtext.scoping.impl.ImportScope;
+import org.eclipse.xtext.xbase.lib.Functions.Function1;
+import org.eclipse.xtext.xbase.lib.IterableExtensions;
+
+import com.google.common.collect.HashMultimap;
+import com.google.common.collect.LinkedHashMultimap;
+import com.google.common.collect.Multimap;
 
 /** Custom import scope that does not trigger resolving imported elements. */
 class NonResolvingImportScope extends ImportScope {
@@ -74,5 +81,51 @@ class NonResolvingImportScope extends ImportScope {
 			}
 		}
 		return result;
+	}
+
+	/*
+	 * Overridden to avoid eager iteration over all elements.
+	 */
+	@Override
+	public Iterable<IEObjectDescription> getAllElements() {
+		final Iterable<IEObjectDescription> globalElements = getParent().getAllElements();
+		final Iterable<IEObjectDescription> aliased = getAllLocalElements();
+		return Iterables2.skipDuplicates(this::getIgnoreCaseAwareQualifiedName, aliased, globalElements);
+	}
+
+	/*
+	 * Overridden to avoid eager iteration over all candidates.
+	 *
+	 * WARNING: this changes the behavior! The implementation of the super class checks for name conflicts across all
+	 * candidates whereas this override checks only on a per-candidate basis.
+	 */
+	@Override
+	protected Iterable<IEObjectDescription> getAliasedElements(Iterable<IEObjectDescription> candidates) {
+		Multimap<QualifiedName, IEObjectDescription> keyToDescription = LinkedHashMultimap.create();
+		Multimap<QualifiedName, ImportNormalizer> keyToNormalizer = HashMultimap.create();
+		return IterableExtensions.flatMap(candidates, new Function1<>() {
+			@Override
+			public Iterable<IEObjectDescription> apply(IEObjectDescription imported) {
+				keyToDescription.clear();
+				keyToNormalizer.clear();
+				QualifiedName fullyQualifiedName = imported.getName();
+				for (ImportNormalizer normalizer : myNormalizers) {
+					QualifiedName alias = normalizer.deresolve(fullyQualifiedName);
+					if (alias != null) {
+						QualifiedName key = alias;
+						if (isIgnoreCase()) {
+							key = key.toLowerCase();
+						}
+						keyToDescription.put(key, new AliasedEObjectDescription(alias, imported));
+						keyToNormalizer.put(key, normalizer);
+					}
+				}
+				for (QualifiedName name : keyToNormalizer.keySet()) {
+					if (keyToNormalizer.get(name).size() > 1)
+						keyToDescription.removeAll(name);
+				}
+				return keyToDescription.values();
+			}
+		});
 	}
 }
