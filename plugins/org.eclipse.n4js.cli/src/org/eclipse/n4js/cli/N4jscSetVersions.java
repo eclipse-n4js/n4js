aa6c2e83ace5ab73037cfa3310d37389718a5248
mmews <marcus.mews@numberfour.eu>
2021-05-19 08:35:31 +0200

    implementation for set-versions

---
+++ plugins/org.eclipse.n4js.cli/src/org/eclipse/n4js/cli/N4jscSetVersions.java
@@ -0,0 +1,23 @@
+/**
+ * Copyright (c) 2021 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.cli;
+
+/**
+ * Performs the n4jsc goal set-versions
+ */
+public class N4jscSetVersions {
+
+	/** Starts the compiler for goal SET-VERSIONS in a blocking fashion */
+	public static N4jscExitState start(N4jscOptions options) {
+		// TODO Auto-generated method stub
+		return N4jscExitState.SUCCESS;
+	}
+}
