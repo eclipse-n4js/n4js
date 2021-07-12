8cb51e12744722bc819e382fe335ae7032c3334f
mmews <marcus.mews@numberfour.eu>
2021-07-12 09:28:06 +0200

    fix more issues

--- plugins/org.eclipse.n4js.json.ide/src/org/eclipse/n4js/json/ide/codeActions/JSONCodeActionService.java
+++ plugins/org.eclipse.n4js.json.ide/src/org/eclipse/n4js/json/ide/codeActions/JSONCodeActionService.java
@@ -53,7 +53,7 @@ public class JSONCodeActionService implements ICodeActionService2 {
 		if (options.getCodeActionParams() != null && options.getCodeActionParams().getContext() != null) {
 			List<Diagnostic> diagnostics = options.getCodeActionParams().getContext().getDiagnostics();
 			for (Diagnostic diag : diagnostics) {
-				if (IssueCodes.NON_EXISTING_PROJECT.equals(diag.getCode())) {
+				if (IssueCodes.NON_EXISTING_PROJECT.equals(diag.getCode().getLeft())) {
 					Command cmd = createInstallNpmCommand(options, diag);
 					if (cmd != null) {
 						result.add(Either.forLeft(cmd));
