03ab7df831dbff9dfe74427516c79b280b44acae
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2021-12-14 14:11:47 +0100

    also convert N4Injector in n4js-runtime to CommonJS

--- n4js-libs/packages/n4js-runtime/npm-bundle.sh
+++ n4js-libs/packages/n4js-runtime/npm-bundle.sh
@@ -12,6 +12,7 @@
 set -e
 cd `dirname $0`
 
+
 rm -f index-bundled.js
 rm -f index-bundled_TEMP.js
 
@@ -23,3 +24,12 @@ rm index-bundled.js
 mv index-bundled_TEMP.js index-bundled.js
 
 echo "Successfully bundled code in folder 'src-gen' into file 'index-bundled.js'."
+
+
+rm -f N4Injector-bundled_TEMP.js
+./node_modules/.bin/esbuild src-gen/n4js/lang/N4Injector.js --bundle --outfile=N4Injector-bundled_TEMP.js --external:n4js-runtime --format=cjs --platform=node
+rm src-gen/n4js/lang/N4Injector.js
+rm -f src-gen/n4js/lang/N4Injector.map
+mv N4Injector-bundled_TEMP.js src-gen/n4js/lang/N4Injector.js
+
+echo "Successfully bundled 'src-gen/n4js/lang/N4Injector.js'."
