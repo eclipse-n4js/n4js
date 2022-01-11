b0dbeeb3e0814979bd21da0b8b9fd229578833fd
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2022-01-11 11:18:45 +0100

    make BinariesLocatorHelper compatible with nvm

---
+++ tests/org.eclipse.n4js.integration.tests/src/org/eclipse/n4js/integration/tests/NodeVersionTest.java
@@ -0,0 +1,40 @@
+/**
+ * Copyright (c) 2022 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.integration.tests;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import org.eclipse.n4js.N4JSGlobals;
+import org.eclipse.n4js.cli.helper.CliTools;
+import org.eclipse.n4js.cli.helper.ProcessResult;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Ensure we are using the correct version of node during the tests.
+ */
+public class NodeVersionTest {
+
+	/***/
+	@Test
+	public void testNodeVersion() {
+		Path cwd = new File("").toPath().toAbsolutePath();
+		ProcessResult result = new CliTools().nodejsRun(cwd, Path.of("-v"));
+		Assert.assertEquals("bad exit code", 0, result.getExitCode());
+		String versionStr = result.getStdOut().trim();
+		if (!N4JSGlobals.isCompatibleNodeVersion(versionStr)) {
+			Assert.fail("tests are running with node version " + versionStr
+					+ " but this version is not compatible to version v" + N4JSGlobals.NODE_VERSION + " defined in "
+					+ N4JSGlobals.class.getSimpleName());
+		}
+	}
+}
