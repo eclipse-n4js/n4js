199652c9f99d7529eb83543901b7977845365014
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2022-01-17 16:02:51 +0100

    another place where .cjs/.mjs file extensions must be registered

--- plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/server/HeadlessExtensionRegistrationHelper.java
+++ plugins/org.eclipse.n4js.ide/src/org/eclipse/n4js/ide/server/HeadlessExtensionRegistrationHelper.java
@@ -10,8 +10,10 @@
  */
 package org.eclipse.n4js.ide.server;
 
+import static org.eclipse.n4js.N4JSGlobals.CJS_FILE_EXTENSION;
 import static org.eclipse.n4js.N4JSGlobals.JSX_FILE_EXTENSION;
 import static org.eclipse.n4js.N4JSGlobals.JS_FILE_EXTENSION;
+import static org.eclipse.n4js.N4JSGlobals.MJS_FILE_EXTENSION;
 import static org.eclipse.n4js.N4JSGlobals.N4JSD_FILE_EXTENSION;
 import static org.eclipse.n4js.N4JSGlobals.N4JSX_FILE_EXTENSION;
 import static org.eclipse.n4js.N4JSGlobals.N4JS_FILE_EXTENSION;
@@ -81,6 +83,8 @@ public class HeadlessExtensionRegistrationHelper implements IHeadlessExtensionRe
 		// Register ECMAScript subgenerator
 		subGeneratorRegistry.register(ecmaScriptSubGenerator, N4JS_FILE_EXTENSION);
 		subGeneratorRegistry.register(ecmaScriptSubGenerator, JS_FILE_EXTENSION);
+		subGeneratorRegistry.register(ecmaScriptSubGenerator, CJS_FILE_EXTENSION);
+		subGeneratorRegistry.register(ecmaScriptSubGenerator, MJS_FILE_EXTENSION);
 		subGeneratorRegistry.register(ecmaScriptSubGenerator, N4JSX_FILE_EXTENSION);
 		subGeneratorRegistry.register(ecmaScriptSubGenerator, JSX_FILE_EXTENSION);
 
