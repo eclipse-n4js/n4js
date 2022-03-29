1bf3cc0cc5116056852b35a0f5fbf4b37d474f57
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2022-03-29 09:15:55 +0200

    add a comment

--- plugins/org.eclipse.n4js.ts.model/emf-gen/org/eclipse/n4js/ts/types/TExportableElement.java
+++ plugins/org.eclipse.n4js.ts.model/emf-gen/org/eclipse/n4js/ts/types/TExportableElement.java
@@ -32,6 +32,11 @@ public interface TExportableElement extends IdentifiableElement {
 	 * Returns the value of the '<em><b>Directly Exported</b></em>' attribute.
 	 * <!-- begin-user-doc -->
 	 * <!-- end-user-doc -->
+	 * <!-- begin-model-doc -->
+	 * Tells whether this element is actually directly exported. This will be true
+	 * iff the corresponding {@code ExportableElement} in the AST returned true
+	 * from {@code #isDeclaredExported()} OR from {@code #isExportedByNamespace()}.
+	 * <!-- end-model-doc -->
 	 * @return the value of the '<em>Directly Exported</em>' attribute.
 	 * @see #setDirectlyExported(boolean)
 	 * @see org.eclipse.n4js.ts.types.TypesPackage#getTExportableElement_DirectlyExported()
