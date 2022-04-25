93e779e0c6cb7577379672b86d7277d8eca190c6
Mark-Oliver Reiser <mark-oliver.reiser@numberfour.eu>
2022-04-25 16:52:15 +0200

    add a test for triple-slash directives

---
+++ tests/org.eclipse.n4js.xpect.tests/xt-tests/dts-parser/user-tests/tripleSlashDirectives/tripleSlashDirectives_index.d.ts
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2022 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+
+/// <reference path="other1.d.ts" />
+/// <reference path="subFolder/other2.d.ts" />
