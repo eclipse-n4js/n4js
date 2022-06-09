a35b522ed565e3fbe90a033180895507c8c8745e
mmews-n4 <marcus.mews@numberfour.eu>
2022-06-09 11:38:59 +0200

    GH-2350: Do not generate return statements in setters stemming from default setter implementations of interfaces (#2371)
    * left-overs from GH-3269
    
    * add test
    
    * fix

---
+++ tests/org.eclipse.n4js.integration.tests/src/org/eclipse/n4js/integration/tests/dts/DtsParseFileTest.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) 2022 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.integration.tests.dts;
+
+import static org.junit.Assert.assertFalse;
+
+import java.io.File;
+
+import org.junit.Ignore;
+import org.junit.Test;
+
+/**
+ * Test for d.ts grammar
+ */
+public class DtsParseFileTest {
+
+	static File FILE = new File("/Users/marcusmews/test/node_modules/@types/node/buffer.d.ts");
+
+	/** Parse .d.ts snippet */
+	@Ignore // use for adhoc-testing
+	@Test
+	public void parseDtsFile() throws Exception {
+		DtsParsesDefinitelyTypedTest.parseFile(FILE.toPath(), (result) -> {
+			assertFalse(result.hasSyntaxErrors());
+		});
+	}
+
+}
