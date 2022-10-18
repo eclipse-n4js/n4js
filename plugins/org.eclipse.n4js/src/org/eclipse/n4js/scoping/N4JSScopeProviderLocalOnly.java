2a0b72d1f0fb346d381bed5bcce75189b1f29cde
mmews <marcus.mews@numberfour.eu>
2022-10-18 16:09:41 +0200

    make class singleton (performance+++)

---
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/scoping/N4JSScopeProviderLocalOnly.java
@@ -0,0 +1,46 @@
+/**
+ * Copyright (c) 2022 NumberFour AG.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ *   NumberFour AG - Initial API and implementation
+ */
+package org.eclipse.n4js.scoping;
+
+import org.eclipse.n4js.utils.TameAutoClosable;
+
+/**
+ *
+ */
+// is not a Singleton due to field 'suppressCrossFileResolutionOfIdentifierRef'
+public class N4JSScopeProviderLocalOnly extends N4JSScopeProvider {
+
+	/** True: Proxies of IdentifierRefs are only resolved within the resource. Otherwise, the proxy is returned. */
+	private boolean suppressCrossFileResolutionOfIdentifierRef = false;
+
+	public TameAutoClosable newCrossFileResolutionSuppressor() {
+		TameAutoClosable tac = new TameAutoClosable() {
+			private boolean tmpSuppressCrossFileResolutionOfIdentifierRef = init();
+
+			private boolean init() {
+				this.tmpSuppressCrossFileResolutionOfIdentifierRef = suppressCrossFileResolutionOfIdentifierRef;
+				suppressCrossFileResolutionOfIdentifierRef = true;
+				return tmpSuppressCrossFileResolutionOfIdentifierRef;
+			}
+
+			@Override
+			public void close() {
+				suppressCrossFileResolutionOfIdentifierRef = tmpSuppressCrossFileResolutionOfIdentifierRef;
+			}
+		};
+		return tac;
+	}
+
+	@Override
+	protected boolean isSuppressCrossFileResolutionOfIdentifierRefs() {
+		return suppressCrossFileResolutionOfIdentifierRef;
+	}
+}
