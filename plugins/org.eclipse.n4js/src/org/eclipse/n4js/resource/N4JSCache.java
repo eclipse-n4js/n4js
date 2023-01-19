6e503c3227e07b213bb99e0405ff0d90c84e4103
mmews <marcus.mews@numberfour.eu>
2023-01-19 13:16:15 +0100

    reason for performance issue?

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/resource/N4JSCache.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/resource/N4JSCache.java
@@ -15,7 +15,6 @@
  */
 package org.eclipse.n4js.resource;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.Objects;
 
@@ -70,12 +69,11 @@ public class N4JSCache extends OnChangeEvictingCache {
 		Preconditions.checkNotNull(firstKey);
 
 		try (Measurement M = N4JSDataCollectors.dcCacheMakeKeys.getMeasurementIfInactive()) {
+			int firstHashCode = firstKey.hashCode();
 			if (moreKeys == null || moreKeys.length == 0) {
-				// must directly return first element because this could be already a key
-				return firstKey;
+				return firstHashCode;
 			}
-			int hashCode = 31 * firstKey.hashCode() + Objects.hash(moreKeys);
-			return Arrays.asList(hashCode, firstKey, Arrays.asList(moreKeys));
+			return 31 * firstHashCode + Objects.hash(moreKeys);
 		}
 	}
 
