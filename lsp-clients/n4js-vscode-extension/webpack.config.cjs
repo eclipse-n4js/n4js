9a7a194c198e965ff2731f448fcdb59699e11266
mmews <marcus.mews@numberfour.eu>
2023-07-05 14:56:34 +0200

    small fix in extension packaging

--- lsp-clients/n4js-vscode-extension/webpack.config.cjs
+++ lsp-clients/n4js-vscode-extension/webpack.config.cjs
@@ -27,7 +27,7 @@ const config = {
     entry: './src-gen/ExtensionProvider.mjs', // the entry point of this extension, 📖 -> https://webpack.js.org/configuration/entry-context/
     output: { // the bundle is stored in the 'dist' folder (check package.json), 📖 -> https://webpack.js.org/configuration/output/
         path: path.resolve(__dirname, 'dist'),
-        filename: 'ExtensionProvider.mjs',
+        filename: 'ExtensionProvider.js',
         libraryTarget: "commonjs2",
         devtoolModuleFilenameTemplate: "../[resource-path]",
     },
