1001a68599900a7b020a969e02a30d164574aeb5
mmews <marcus.mews@numberfour.eu>
2024-02-13 08:45:47 +0100

    some migrations

--- plugins/org.eclipse.n4js/src/org/eclipse/n4js/formatting2/TypeExpressionFormatterNoOp.java
+++ plugins/org.eclipse.n4js/src/org/eclipse/n4js/formatting2/TypeExpressionFormatterNoOp.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2016 NumberFour AG.
+ * Copyright (c) 2024 NumberFour AG.
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
@@ -10,16 +10,15 @@
  */
 package org.eclipse.n4js.formatting2;
 
-import org.eclipse.xtext.formatting2.AbstractFormatter2
-import org.eclipse.xtext.formatting2.IFormattableDocument
+import org.eclipse.xtext.formatting2.IFormattableDocument;
 
-abstract class TypeExpressionsFormatter extends AbstractFormatter2 {
-}
-
+/**
+ *
+ */
 public class TypeExpressionFormatterNoOp extends TypeExpressionsFormatter {
 
-	override format(Object obj, IFormattableDocument document) {
-		throw new UnsupportedOperationException("TypeExpressionFormatter should not be used directly.")
+	@Override
+	public void format(Object obj, IFormattableDocument document) {
+		throw new UnsupportedOperationException("TypeExpressionFormatter should not be used directly.");
 	}
-
 }
