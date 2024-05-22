7f27b5078c40ab99ab6bb1bcb35723a872262dbf
mmews-n4 <marcus.mews@numberfour.eu>
2024-05-22 15:38:50 +0200

    GH-2627: d.ts parser fails on specific input (#2628)
    * update antlr4
    
    * fix grammar
    
    * generate grammar
    
    * update antlr4 (amend)
    
    * update gitignore
    
    * add test
    
    * remove complete jar due to maven problem
    
    * adjust gitignore
    
    * adjust due to antlr-runtime upgrade

--- plugins/org.eclipse.n4js.dts/src-gen-antlr/org/eclipse/n4js/dts/TypeScriptParserBaseListener.java
+++ plugins/org.eclipse.n4js.dts/src-gen-antlr/org/eclipse/n4js/dts/TypeScriptParserBaseListener.java
@@ -1,4 +1,4 @@
-// Generated from /Users/marcusmews/Eclipses/ECL1/git/n4js/plugins/org.eclipse.n4js.dts/grammar/TypeScriptParser.g4 by ANTLR 4.7.2
+// Generated from /Users/marcusmews/Eclipses/ECL3/git/n4js/plugins/org.eclipse.n4js.dts/grammar/TypeScriptParser.g4 by ANTLR 4.13.1
 package org.eclipse.n4js.dts;
 
 import org.antlr.v4.runtime.ParserRuleContext;
@@ -10,6 +10,7 @@ import org.antlr.v4.runtime.tree.TerminalNode;
  * which can be extended to create a listener which only needs to handle a subset
  * of the available methods.
  */
+@SuppressWarnings("CheckReturnValue")
 public class TypeScriptParserBaseListener implements TypeScriptParserListener {
 	/**
 	 * {@inheritDoc}
